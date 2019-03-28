8194932be79eb6c1fb05064110ca3fe5c24e47b0
Ketill Fenne <ketill.fenne@gmail.com>
2019-03-28 09:43:10 +0100

    Ut med bouncycastle og ryddet i POMs

--- apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/tokensupport/JwtTokenGenerator.java
+++ apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/tokensupport/JwtTokenGenerator.java
@@ -2,17 +2,16 @@ package no.nav.data.catalog.backend.app.common.tokensupport;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemReader;
+import no.nav.data.catalog.backend.app.common.exceptions.DataCatalogBackendTechnicalException;
+import org.apache.commons.codec.binary.Base64;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Date;
@@ -21,14 +20,14 @@ import java.util.Map;
 
 public class JwtTokenGenerator {
 
-    public static String generateToken(String filePath) {
-        java.security.Security.addProvider(
-                new org.bouncycastle.jce.provider.BouncyCastleProvider());
+    private static final String PKCS_1_PEM_HEADER = "-----BEGIN RSA PRIVATE KEY-----";
+    private static final String PKCS_1_PEM_FOOTER = "-----END RSA PRIVATE KEY-----";
 
-        PrivateKey privateKey = loadKey(filePath);
-
-        String token = null;
+    public static String generateToken(String filePath)  {
+        PrivateKey privateKey;
+        String token;
         try {
+            privateKey = loadKey(filePath);
             Map<String, Object> claims = new HashMap<String, Object>();
             Date now = new Date();
             //seconds
@@ -42,49 +41,57 @@ public class JwtTokenGenerator {
             claims.put("iss", "26100");
 
             token = Jwts.builder().setClaims(claims).signWith(SignatureAlgorithm.RS256, privateKey).compact();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (GeneralSecurityException e) {
+            throw new DataCatalogBackendTechnicalException("General security exception occured reading private key file: " + filePath, e);
+        } catch (IOException e) {
+            throw new DataCatalogBackendTechnicalException("Error occured reading private key file: " + filePath, e);
         }
         return token;
     }
 
-    private static PrivateKey loadKey(String filepath) {
-        PrivateKey key = null;
-        try {
-            key = readPrivateKeyFromFile(filepath, "RSA");
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+    private static PrivateKey loadKey(String keyFilePath) throws GeneralSecurityException, IOException {
+        byte[] keyDataBytes = Files.readAllBytes(Paths.get(keyFilePath));
+        String keyDataString = new String(keyDataBytes, StandardCharsets.UTF_8);
+
+        if (keyDataString.contains(PKCS_1_PEM_HEADER)) {
+            // OpenSSL / PKCS#1 Base64 PEM encoded file
+            keyDataString = keyDataString.replace(PKCS_1_PEM_HEADER, "");
+            keyDataString = keyDataString.replace(PKCS_1_PEM_FOOTER, "");
+            return readPkcs1PrivateKey(Base64.decodeBase64(keyDataString));
         }
-        return key;
+
+        // We assume it's a PKCS#8 DER encoded binary file
+        return readPkcs8PrivateKey(Files.readAllBytes(Paths.get(keyFilePath)));
     }
 
-    private static byte[] parsePEMFile(File pemFile) throws IOException {
-        if (!pemFile.isFile() || !pemFile.exists()) {
-            throw new FileNotFoundException(String.format("The file '%s' doesn't exist.", pemFile.getAbsolutePath()));
-        }
-        PemReader reader = new PemReader(new FileReader(pemFile));
-        PemObject pemObject = reader.readPemObject();
-        byte[] content = pemObject.getContent();
-        reader.close();
-        return content;
-    }
-
-    private static PrivateKey getPrivateKey(byte[] keyBytes, String algorithm) {
-        PrivateKey privateKey = null;
+    private static PrivateKey readPkcs8PrivateKey(byte[] pkcs8Bytes) throws GeneralSecurityException {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA", "SunRsaSign");
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pkcs8Bytes);
         try {
-            KeyFactory kf = KeyFactory.getInstance(algorithm);
-            EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-            privateKey = kf.generatePrivate(keySpec);
-        } catch (NoSuchAlgorithmException e) {
-            System.out.println("Could not reconstruct the private key, the given algorithm could not be found.");
+            return keyFactory.generatePrivate(keySpec);
         } catch (InvalidKeySpecException e) {
-            System.out.println("Could not reconstruct the private key");
+            throw new IllegalArgumentException("Unexpected key format!", e);
         }
-        return privateKey;
     }
 
-    private static PrivateKey readPrivateKeyFromFile(String filepath, String algorithm) throws IOException {
-        byte[] bytes = parsePEMFile(new File(filepath));
-        return getPrivateKey(bytes, algorithm);
+    private static PrivateKey readPkcs1PrivateKey(byte[] pkcs1Bytes) throws GeneralSecurityException {
+        // We can't use Java internal APIs to parse ASN.1 structures, so we build a PKCS#8 key Java can understand
+        int pkcs1Length = pkcs1Bytes.length;
+        int totalLength = pkcs1Length + 22;
+        byte[] pkcs8Header = new byte[] {
+                0x30, (byte) 0x82, (byte) ((totalLength >> 8) & 0xff), (byte) (totalLength & 0xff), // Sequence + total length
+                0x2, 0x1, 0x0, // Integer (0)
+                0x30, 0xD, 0x6, 0x9, 0x2A, (byte) 0x86, 0x48, (byte) 0x86, (byte) 0xF7, 0xD, 0x1, 0x1, 0x1, 0x5, 0x0, // Sequence: 1.2.840.113549.1.1.1, NULL
+                0x4, (byte) 0x82, (byte) ((pkcs1Length >> 8) & 0xff), (byte) (pkcs1Length & 0xff) // Octet string + length
+        };
+        byte[] pkcs8bytes = join(pkcs8Header, pkcs1Bytes);
+        return readPkcs8PrivateKey(pkcs8bytes);
+    }
+
+    private static byte[] join(byte[] byteArray1, byte[] byteArray2){
+        byte[] bytes = new byte[byteArray1.length + byteArray2.length];
+        System.arraycopy(byteArray1, 0, bytes, 0, byteArray1.length);
+        System.arraycopy(byteArray2, 0, bytes, byteArray1.length, byteArray2.length);
+        return bytes;
     }
 }
