38e181052a8cbce263095db8fb64cce263b372e2
elmgren <marten.samuel.elmgren@nav.no>
2019-04-10 12:52:48 +0200

    Created LocalAppStarter to be used in development with docker-compose

--- apps/backend/data-catalog-backend-app/src/test/java/no/nav/data/catalog/backend/app/LocalAppStarter.java
+++ apps/backend/data-catalog-backend-app/src/test/java/no/nav/data/catalog/backend/app/LocalAppStarter.java
@@ -1,19 +1,15 @@
 package no.nav.data.catalog.backend.app;
 
 
-//@ActiveProfiles("local")
-public class LocalAppStarter {
-	public static void main(String[] args){
-		System.setProperty("spring.profiles.active", "local");
-//
-//		try (InputStream inputStream = new FileInputStream(getFile(getenv("DATACATALOG_BACKEND_CONFIG") + "datacatalog-backend.properties"))) {
-//			Properties properties = new Properties();
-//			properties.load(inputStream);
-//			properties.forEach((key, value) -> System.setProperty(key.toString(), value.toString()));
-//		} catch (IOException e) {
-//			throw new RuntimeException("Cannot find configuration on PATH");
-//		}
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("local")
+@Import(AppStarter.class)
+public class LocalAppStarter {
+
+	public static void main(String[] args) {
+		System.setProperty("spring.profiles.active", "local");
 		AppStarter.main(args);
 	}
 }
