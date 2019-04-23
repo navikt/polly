82d1cacbffc7fc6597c2220d7cd755e7206d9ccc
Ketill Fenne <ketill.fenne@gmail.com>
2019-04-23 13:15:25 +0200

    Integration test for consumig file from Github, processing, and store to ElasticSearch

--- apps/backend/data-catalog-backend-test/data-catalog-backend-test-integration/src/test/java/no/nav/data/catalog/backend/test/integration/GetFromGithubSaveElasticsearchIT.java
+++ apps/backend/data-catalog-backend-test/data-catalog-backend-test-integration/src/test/java/no/nav/data/catalog/backend/test/integration/GetFromGithubSaveElasticsearchIT.java
@@ -1,56 +1,72 @@
 package no.nav.data.catalog.backend.test.integration;
 
-import no.nav.data.catalog.backend.app.github.GithubConsumer;
+import no.nav.data.catalog.backend.app.common.exceptions.DataCatalogBackendTechnicalException;
 import no.nav.data.catalog.backend.app.github.GithubService;
 import no.nav.data.catalog.backend.app.record.Record;
 import no.nav.data.catalog.backend.app.record.RecordService;
+import no.nav.data.catalog.backend.test.component.FixedElasticsearchContainer;
 import org.elasticsearch.ElasticsearchStatusException;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-
-public class GetFromGithubSaveElasticsearchIT {
-
-    @Autowired
-    private GithubConsumer consumerMock;
-
-    @Autowired
-    private RecordService recordService;
+public class GetFromGithubSaveElasticsearchIT extends IntegrationTestBase {
 
     @Autowired
     private GithubService service;
 
-//    @ClassRule
-//    public static FixedElasticsearchContainer container = new FixedElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch-oss:6.4.1");
+    @Autowired
+    private RecordService recordService;
+
+    @ClassRule
+    public static FixedElasticsearchContainer container = new FixedElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch-oss:6.4.1");
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
+    @Test
+    public void retriveAndSaveSingleDataset() throws Exception {
+        deleteAllFromElasticsearch();
+
+        service.handle("testdataIkkeSlett/singleRow.json");
+        //Give elasticsearch a few seconds to index documents
+        Thread.sleep(2000L);
+        List<Record> recordList = recordService.getAllRecords();
+        assertEquals(1, recordList.size());
+    }
 
     @Test
     public void retriveAndSaveMultipleDataset() throws Exception {
         deleteAllFromElasticsearch();
-        byte[] content = null;
-        try (InputStream in = getClass().getResourceAsStream("/files/InformationTypes.json")) {
-            content = in.readAllBytes();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         service.handle("testdataIkkeSlett/multipleRows.json");
         //Give elasticsearch a few seconds to index documents
         Thread.sleep(2000L);
         List<Record> recordList = recordService.getAllRecords();
         assertEquals(6, recordList.size());
+    }
+
+    @Test
+    public void retriveInvalidFile() throws Exception {
+        expectedException.expect(DataCatalogBackendTechnicalException.class);
+        expectedException.expectMessage("Error occurred during parse of Json in file invalidFile.json from github");
+
+        service.handle("testdataIkkeSlett/invalidFile.json");
+    }
+
+    @Test
+    public void retriveNotExistingFile() throws Exception {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Calling Github to download file failed with status=404 NOT_FOUND. The file does not exist");
+
+        service.handle("testdataIkkeSlett/notExistingFile.json");
     }
 
     private void deleteAllFromElasticsearch() {
--- apps/backend/data-catalog-backend-test/data-catalog-backend-test-integration/src/test/java/no/nav/data/catalog/backend/test/integration/IntegrationTestConfig.java
+++ apps/backend/data-catalog-backend-test/data-catalog-backend-test-integration/src/test/java/no/nav/data/catalog/backend/test/integration/IntegrationTestConfig.java
@@ -1,4 +1,11 @@
 package no.nav.data.catalog.backend.test.integration;
 
+import no.nav.data.catalog.backend.app.AppStarter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication (scanBasePackageClasses = {
+        AppStarter.class,
+        IntegrationTestConfig.class
+        })
 public class IntegrationTestConfig {
 }
