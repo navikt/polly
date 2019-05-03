2fc33d4da7ba0cdd0754c2b426ff5d11177623fd
Ketill Fenne <ketill.fenne@gmail.com>
2019-05-03 08:54:56 +0200

    Added auditing to entities

---
+++ apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/auditing/AuditorAwareImpl.java
@@ -0,0 +1,13 @@
+package no.nav.data.catalog.backend.app.common.auditing;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Datajeger");
+        // Use below commented code when will use Spring Security.
+    }
+}
