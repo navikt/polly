884186b07ca0cdf2618666037dfa24364b3d34d8
elmgren <marten.samuel.elmgren@nav.no>
2019-06-11 10:00:55 +0200

    Fix mergeConflicts

--- apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/informationtype/InformationTypeController.java
+++ apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/informationtype/InformationTypeController.java
@@ -11,7 +11,6 @@ import no.nav.data.catalog.backend.app.elasticsearch.ElasticsearchStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +22,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
