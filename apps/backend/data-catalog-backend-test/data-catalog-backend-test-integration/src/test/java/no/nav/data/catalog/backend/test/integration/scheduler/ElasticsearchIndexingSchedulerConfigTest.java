6278e7f810abc231b807f8708d9023e50e22b6b6
Andreas Skomedal <andreas.skomedal@nav.no>
2019-08-09 11:39:19 +0200

    disable banner on hibernate types

--- apps/backend/data-catalog-backend-test/data-catalog-backend-test-integration/src/test/java/no/nav/data/catalog/backend/test/integration/scheduler/ElasticsearchIndexingSchedulerConfigTest.java
+++ apps/backend/data-catalog-backend-test/data-catalog-backend-test-integration/src/test/java/no/nav/data/catalog/backend/test/integration/scheduler/ElasticsearchIndexingSchedulerConfigTest.java
@@ -1,33 +1,19 @@
 package no.nav.data.catalog.backend.test.integration.scheduler;
 
-import no.nav.data.catalog.backend.app.AppStarter;
 import no.nav.data.catalog.backend.app.informationtype.InformationTypeService;
-import no.nav.data.catalog.backend.test.component.PostgresTestContainer;
+import no.nav.data.catalog.backend.test.integration.IntegrationTestBase;
 import org.awaitility.Duration;
-import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.verify;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = AppStarter.class)
-@ActiveProfiles("test")
-@ContextConfiguration(initializers = {PostgresTestContainer.Initializer.class})
-public class ElasticsearchIndexingSchedulerConfigTest {
+public class ElasticsearchIndexingSchedulerConfigTest extends IntegrationTestBase {
 
     @SpyBean
     private InformationTypeService service;
-
-    @ClassRule
-    public static PostgresTestContainer postgresTestContainer = PostgresTestContainer.getInstance();
 
     @Test
     public void jobRuns() {
