dca1b25e72a30ceaeb3a214baeb4960c3072e8e6
elmgren <marten.samuel.elmgren@nav.no>
2019-09-04 10:59:06 +0200

    removed equals and hashCode methods because they are generated with the @Data annotation

--- apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/validator/ValidationError.java
+++ apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/validator/ValidationError.java
@@ -20,32 +20,4 @@ public class ValidationError implements Serializable {
     public String toString() {
         return String.format("%s -- %s -- %s", reference, errorType, errorMessage);
     }
-
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-
-        if (other == null || other.getClass() != this.getClass()) {
-            return false;
-        }
-
-        ValidationError otherValidationError = (ValidationError) other;
-        return (reference != null && reference.equals(otherValidationError.getReference())) &&
-                (errorType != null && errorType.equals(otherValidationError.getErrorType())) &&
-                (errorMessage != null && errorMessage.equals(otherValidationError
-                        .getErrorMessage()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((reference == null) ? 0 : reference.hashCode());
-        result = prime * result + ((errorType == null) ? 0 : errorType.hashCode());
-        result = prime * result + ((errorMessage == null) ? 0 : errorMessage.hashCode());
-        return result;
-    }
 }
