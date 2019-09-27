187d7a09703f590bb976a15e75ba20ad3f8707ee
Andreas Skomedal <andreas.skomedal@nav.no>
2019-09-27 16:21:32 +0200

    Update to stateless auth filter

--- apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/security/WebSecurityConfig.java
+++ apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/security/WebSecurityConfig.java
@@ -1,26 +1,27 @@
 package no.nav.data.catalog.backend.app.common.security;
 
-import com.microsoft.azure.spring.autoconfigure.aad.AADAuthenticationFilter;
+import com.microsoft.azure.spring.autoconfigure.aad.AADAppRoleStatelessAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired(required = false)
-    private AADAuthenticationFilter aadAuthFilter;
+    private AADAppRoleStatelessAuthenticationFilter aadAuthFilter;
     @Value("${security.enabled:true}")
     private boolean enable;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         if (!enable) {
             return;
@@ -34,16 +35,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // Verified by github signature
         http.authorizeRequests().antMatchers("/webhooks/**").permitAll();
 
-        http.authorizeRequests().antMatchers(HttpMethod.GET,"/dataset/**").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET,"/codelist/**").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET,"/distributionchannel/**").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET,"/system/**").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET,"/records/search").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/dataset/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/codelist/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/distributionchannel/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/system/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/records/search").permitAll();
 
         http.authorizeRequests().anyRequest().authenticated();
-
-        http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/").deleteCookies("JSESSIONID").invalidateHttpSession(true);
 
         http.addFilterBefore(aadAuthFilter, UsernamePasswordAuthenticationFilter.class);
     }
