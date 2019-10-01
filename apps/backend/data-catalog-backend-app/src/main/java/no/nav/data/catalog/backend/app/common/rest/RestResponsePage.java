b77f9962e00d7a5a40f23ff79ac7989135bc0731
Andreas Skomedal <andreas.skomedal@nav.no>
2019-10-01 16:21:01 +0200

    fiksa fieldnavn, rekkefølge, rest paging response

--- apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/rest/RestResponsePage.java
+++ apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/rest/RestResponsePage.java
@@ -1,5 +1,6 @@
 package no.nav.data.catalog.backend.app.common.rest;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -8,17 +9,18 @@ import java.util.List;
 
 @Getter
 @NoArgsConstructor
+@JsonPropertyOrder({"pageNumber", "pageSize", "returnedElements", "totalElements", "content"})
 public class RestResponsePage<T> {
 
     private List<T> content;
-    private int currentPage;
+    private int pageNumber;
     private int pageSize;
     private long returnedElements;
     private long totalElements;
 
     public RestResponsePage(List<T> content, Pageable pageable, long total) {
         this.content = content;
-        this.currentPage = pageable.getPageNumber();
+        this.pageNumber = pageable.getPageNumber();
         this.pageSize = pageable.getPageSize();
         this.returnedElements = content.size();
         this.totalElements = total;
