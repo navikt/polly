820a0a90fbb57027d439e34404adb94997943bcd
Andreas Skomedal <andreas.skomedal@nav.no>
2019-10-08 11:57:32 +0200

    Auditor from azure authentication

--- apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/auditing/AuditorAwareImpl.java
+++ apps/backend/data-catalog-backend-app/src/main/java/no/nav/data/catalog/backend/app/common/auditing/AuditorAwareImpl.java
@@ -1,13 +1,23 @@
 package no.nav.data.catalog.backend.app.common.auditing;
 
+import com.microsoft.azure.spring.autoconfigure.aad.UserPrincipal;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
+
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("Datajeger");
-        // Use below commented code when will use Spring Security.
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(authentication -> (UserPrincipal) authentication.getPrincipal())
+                .map(principal -> String.format("%s - %s", getNAVident(principal), principal.getName()))
+                .or(() -> Optional.of("Datajeger"));
+    }
+
+    private String getNAVident(UserPrincipal principal) {
+        Object navClaim = principal.getClaim("NAVident");
+        return navClaim == null ? "missing-ident" : ((String) navClaim);
     }
 }
