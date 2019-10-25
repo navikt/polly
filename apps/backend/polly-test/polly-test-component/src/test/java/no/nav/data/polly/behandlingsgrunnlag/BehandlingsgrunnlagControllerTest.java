beff376351fda800bf3ff56351c0db715253079e
Andreas Skomedal <andreas.skomedal@nav.no>
2019-10-25 11:38:31 +0200

    BehandlingsgrunnlagControllerTest

--- apps/backend/polly-test/polly-test-component/src/test/java/no/nav/data/polly/behandlingsgrunnlag/BehandlingsgrunnlagControllerTest.java
+++ apps/backend/polly-test/polly-test-component/src/test/java/no/nav/data/polly/behandlingsgrunnlag/BehandlingsgrunnlagControllerTest.java
@@ -1,8 +1,8 @@
 package no.nav.data.polly.behandlingsgrunnlag;
 
 import no.nav.data.polly.AppStarter;
-import no.nav.data.polly.policy.PolicyService;
-import no.nav.data.polly.policy.entities.Policy;
+import no.nav.data.polly.behandlingsgrunnlag.domain.InformationTypeBehandlingsgrunnlagResponse;
+import no.nav.data.polly.legalbasis.LegalBasisResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +12,15 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(BehandlingsgrunnlagController.class)
@@ -28,20 +30,23 @@ class BehandlingsgrunnlagControllerTest {
 
     @Autowired
     private MockMvc mvc;
-
     @MockBean
-    private PolicyService policyService;
+    private BehandlingsgrunnlagService behandlingsgrunnlagService;
 
     @Test
     void hentBehandlingsgrunnlag() throws Exception {
-        given(policyService.findActiveByPurposeCode("the-purpose"))
-                .willReturn(List.of(Policy.builder()/*.datasetId("id").datasetTitle("title").legalBasisDescription("legaldesc")*/.build()));
+        given(behandlingsgrunnlagService.findBehandlingForPurpose("the-purpose"))
+                .willReturn(List.of(new InformationTypeBehandlingsgrunnlagResponse(UUID.fromString("ebd6296f-dbfb-4be3-82ea-16f2cc64f031"),
+                        "name", List.of(LegalBasisResponse.builder().gdpr("gdpr").nationalLaw("law").description("legaldesc").build()))
+                ));
 
         mvc.perform(get("/behandlingsgrunnlag/purpose/{purpose}", "the-purpose"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.purpose", is("the-purpose")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.datasets[0].id", is("id")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.datasets[0].title", is("title")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.datasets[0].legalBasisDescription", is("legaldesc")));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.purpose", is("the-purpose")))
+                .andExpect(jsonPath("$.informationTypes[0].id", is("ebd6296f-dbfb-4be3-82ea-16f2cc64f031")))
+                .andExpect(jsonPath("$.informationTypes[0].name", is("name")))
+                .andExpect(jsonPath("$.informationTypes[0].legalBases[0].gdpr", is("gdpr")))
+                .andExpect(jsonPath("$.informationTypes[0].legalBases[0].nationalLaw", is("law")))
+                .andExpect(jsonPath("$.informationTypes[0].legalBases[0].description", is("legaldesc")));
     }
 }
