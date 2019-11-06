7baad9d569626c4b3d06920c59384b72eaa9272e
danishnav <danish.khan@nav.no>
2019-11-06 10:43:55 +0100

    ryddet i mappestruktur

--- apps/frontend/src/mock/purposeMock.tsx
+++
@@ -1,5 +0,0 @@
-// Used in Select components options
-export default [
-    { id: 'Statistikk'  },
-    { id: 'Foreldrepenger' }
-]
--- apps/frontend/src/routes.tsx
+++ apps/frontend/src/routes.tsx
@@ -2,21 +2,13 @@ import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Root from "./components/Root";
-import CreatePage from "./pages/CreatePage";
-import EditPage from "./pages/EditPage";
 import PurposePage from "./pages/PurposePage";
-import TempPurposePage from "./pages/temp/TempPurposePage";
-import TempPurposePageSec from "./pages/temp/TempPurposePageSec";
 import InformationtypeCreatePage from "./pages/InformationtypeCreatePage";
 import InformationtypeEditPage from "./pages/InformationtypeEditPage";
 
 const Routes = (): JSX.Element => (
     <Root>
         <Switch>
-            <Route exact path="/tempPurpose" component={TempPurposePage} />
-            <Route exact path="/tempPurpose2" component={TempPurposePageSec} />
-            <Route exact path="/create" component={CreatePage} />
-            <Route exact path="/edit/:id" component={EditPage} />
             <Route exact path="/purpose" component={PurposePage} />
 
             <Route
