65ce094167987bfeaed2682f669887632dfaff98
Andreas Skomedal <andreas.skomedal@nav.no>
2019-11-14 11:59:10 +0100

    minor CodelistStub cleanup

--- apps/backend/polly-test/polly-test-component/src/test/java/no/nav/data/polly/codelist/CodelistStub.java
+++ apps/backend/polly-test/polly-test-component/src/test/java/no/nav/data/polly/codelist/CodelistStub.java
@@ -7,29 +7,33 @@ public class CodelistStub {
 
     public static void initializeCodelist() {
         CodelistCache.init();
-        CodelistCache.set(create(ListName.SOURCE, "ARBEIDSGIVER", "Arbeidsgiver"));
-        CodelistCache.set(create(ListName.SOURCE, "SKATTEETATEN", "Skatteetaten"));
-        CodelistCache.set(create(ListName.SOURCE, "BRUKER", "BRUKER"));
-        CodelistCache.set(create(ListName.SOURCE, "Skatt", "Skatteetaten"));
-        CodelistCache.set(create(ListName.CATEGORY, "Personalia", "Personalia"));
-        CodelistCache.set(create(ListName.CATEGORY, "ARBEIDSFORHOLD", "Arbeidsforhold"));
-        CodelistCache.set(create(ListName.CATEGORY, "UTDANNING", "Utdanning"));
-        CodelistCache.set(create(ListName.PURPOSE, "Kontroll", "Kontrollering"));
-        CodelistCache.set(create(ListName.PURPOSE, "AAP", "Arbeidsavklaringspenger"));
-        CodelistCache.set(create(ListName.NATIONAL_LAW, "Samtykke", ""));
-        CodelistCache.set(create(ListName.NATIONAL_LAW, "Ftrl", "1997-02-28-19"));
-        CodelistCache.set(create(ListName.NATIONAL_LAW, "Ny alderspensjon", "2009-06-05-32"));
-        CodelistCache.set(create(ListName.SENSITIVITY, "Særlige personopplysninger", "Særlige kategorier av personopplysninger"));
-        CodelistCache.set(create(ListName.SENSITIVITY, "Personopplysning", "Personopplysning"));
-        CodelistCache.set(create(ListName.SUBJECT_CATEGORY, "Bruker", "Bruker"));
-        CodelistCache.set(create(ListName.GDPR_ARTICLE, "6a", "6a"));
-        CodelistCache.set(create(ListName.GDPR_ARTICLE, "6e", "6e"));
-        CodelistCache.set(create(ListName.GDPR_ARTICLE, "9a", "9a"));
-        CodelistCache.set(create(ListName.DEPARTMENT, "dep", "mdep"));
-        CodelistCache.set(create(ListName.SUB_DEPARTMENT, "subdep", "sdep"));
-        CodelistCache.set(create(ListName.SYSTEM, "TPS", "Tjenestebasert PersondataSystem"));
-        CodelistCache.set(create(ListName.SYSTEM, "PESYS", "Pensjonssystem"));
-        CodelistCache.set(create(ListName.SYSTEM, "AA-REG", "Arbeidsgiver / Arbeidstaker register"));
+        add(ListName.SOURCE, "ARBEIDSGIVER", "Arbeidsgiver");
+        add(ListName.SOURCE, "SKATTEETATEN", "Skatteetaten");
+        add(ListName.SOURCE, "BRUKER", "BRUKER");
+        add(ListName.SOURCE, "Skatt", "Skatteetaten");
+        add(ListName.CATEGORY, "Personalia", "Personalia");
+        add(ListName.CATEGORY, "ARBEIDSFORHOLD", "Arbeidsforhold");
+        add(ListName.CATEGORY, "UTDANNING", "Utdanning");
+        add(ListName.PURPOSE, "Kontroll", "Kontrollering");
+        add(ListName.PURPOSE, "AAP", "Arbeidsavklaringspenger");
+        add(ListName.NATIONAL_LAW, "Samtykke", "");
+        add(ListName.NATIONAL_LAW, "Ftrl", "1997-02-28-19");
+        add(ListName.NATIONAL_LAW, "Ny alderspensjon", "2009-06-05-32");
+        add(ListName.SENSITIVITY, "Særlige personopplysninger", "Særlige kategorier av personopplysninger");
+        add(ListName.SENSITIVITY, "Personopplysning", "Personopplysning");
+        add(ListName.SUBJECT_CATEGORY, "Bruker", "Bruker");
+        add(ListName.GDPR_ARTICLE, "6a", "6a");
+        add(ListName.GDPR_ARTICLE, "6e", "6e");
+        add(ListName.GDPR_ARTICLE, "9a", "9a");
+        add(ListName.DEPARTMENT, "dep", "mdep");
+        add(ListName.SUB_DEPARTMENT, "subdep", "sdep");
+        add(ListName.SYSTEM, "TPS", "Tjenestebasert PersondataSystem");
+        add(ListName.SYSTEM, "PESYS", "Pensjonssystem");
+        add(ListName.SYSTEM, "AA-REG", "Arbeidsgiver / Arbeidstaker register");
+    }
+
+    private static void add(ListName source, String arbeidsgiver, String arbeidsgiver2) {
+        CodelistCache.set(create(source, arbeidsgiver, arbeidsgiver2));
     }
 
     private static Codelist create(ListName list, String code, String description) {
