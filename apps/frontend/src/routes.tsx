7014650f24b52720225c1527c96bb06589b1e9a2
Yazdanmehr, Shayan <Shayan.Yazdanmehr@nav.no>
2019-11-29 10:19:22 +0100

    Add codelist page routes

--- apps/frontend/src/routes.tsx
+++ apps/frontend/src/routes.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 
 import Root from "./components/Root";
 import PurposePage from "./pages/PurposePage";
 import InformationtypeCreatePage from "./pages/InformationtypeCreatePage";
 import InformationtypeEditPage from "./pages/InformationtypeEditPage";
 import InformationtypePage from './pages/InformationtypePage'
-import { Main } from "./pages/MainPage"
+import {Main} from "./pages/MainPage"
+import CodelistPage from "./pages/CodelistPage"
 
 
-const Routes = (): JSX.Element => (
+const Routes = (props: any): JSX.Element => (
   <Root>
     <Switch>
       <Route exact path="/purpose/:id?/:processid?" component={PurposePage} />
-
       <Route
         exact
         path="/informationtype/create"
@@ -29,9 +29,15 @@ const Routes = (): JSX.Element => (
         path="/informationtype/edit/:id"
         component={InformationtypeEditPage}
       />
+      <Route
+        exact
+        path="/admin/codelist"
+        component={CodelistPage}
+      />
       <Route>
         <Main/>
       </Route>
+
     </Switch>
   </Root>
 );
