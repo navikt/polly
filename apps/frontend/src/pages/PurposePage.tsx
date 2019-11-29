a293ddda876c7fcef2a789f87d152b4cc5d6491a
Andreas Skomedal <andreas.skomedal@nav.no>
2019-11-29 14:59:50 +0100

    illustrations alt fix

--- apps/frontend/src/pages/PurposePage.tsx
+++ apps/frontend/src/pages/PurposePage.tsx
@@ -125,7 +125,7 @@ const PurposePage = (props: any) => {
                 </React.Fragment>
             ) :
                 <Block display="flex" justifyContent="center" alignContent="center" marginTop={theme.sizing.scale4800}>
-                    <img src={illustration} alt={intl.startIllustration} style={{maxWidth: "65%"}}/>
+                    <img src={illustration} alt={intl.treasureIllustration} style={{maxWidth: "65%"}}/>
                 </Block>
             }
         </React.Fragment>
