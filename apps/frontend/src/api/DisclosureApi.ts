4228fd7d4f7f3c85b317ee5ab06ebe57791b617c
Andreas Skomedal <andreas.skomedal@nav.no>
2020-01-07 16:52:49 +0100

    improve code usage links

--- apps/frontend/src/api/DisclosureApi.ts
+++ apps/frontend/src/api/DisclosureApi.ts
@@ -4,16 +4,20 @@ import { PageResponse, DisclosureFormValues, Disclosure } from "../constants";
 const server_polly = process.env.REACT_APP_POLLY_ENDPOINT;
 
 export const getAllDisclosures = async () => {
-    return (await axios.get(`${server_polly}/disclosure?pageSize=250`)).data.content;
+    return (await axios.get<PageResponse<Disclosure>>(`${server_polly}/disclosure?pageSize=250`)).data.content;
+};
+
+export const getDisclosure = async (disclosureId: string) => {
+    return (await axios.get<Disclosure>(`${server_polly}/disclosure/${disclosureId}`)).data;
 };
 
 export const getDisclosuresByRecipient = async (recipient: string) => {
-    return (await axios.get(`${server_polly}/disclosure/?recipient=${recipient}`)).data.content
+    return (await axios.get<PageResponse<Disclosure>>(`${server_polly}/disclosure/?recipient=${recipient}`)).data.content
 }
 
 export const createDisclosure = async (disclosure: DisclosureFormValues) => {
     let body = mapDisclosureFromForm(disclosure);
-    return (await axios.post(`${server_polly}/disclosure`, body)).data;
+    return (await axios.post<Disclosure>(`${server_polly}/disclosure`, body)).data;
 };
 
 export const mapDisclosureFromForm = (values: DisclosureFormValues) => {
