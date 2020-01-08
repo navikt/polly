c01ca5f9d88c723f5aef00f2b2e2f87a34ab1def
Andreas Skomedal <andreas.skomedal@nav.no>
2020-01-08 17:20:40 +0100

    hide non-occuring usage types in code view

--- apps/frontend/src/components/CodeList/CodeListUsage.tsx
+++ apps/frontend/src/components/CodeList/CodeListUsage.tsx
@@ -11,23 +11,19 @@ import { Label1, Label2 } from "baseui/typography"
 export const Usage = (props: { usage: CodeUsage }) => {
     const {usage} = props;
     const maxRows = Math.max(usage.disclosures.length, usage.informationTypes.length, usage.processes.length, usage.policies.length)
+    const informationTypes = !!usage.informationTypes.length
+    const processes = !!usage.processes.length
+    const policies = !!usage.policies.length
+    const disclosures = !!usage.disclosures.length
     return (
         <Block marginTop="2rem">
             <Label2 font="font450" marginBottom=".5rem">{intl.usage}</Label2>
             <StyledTable>
                 <StyledHead>
-                    <StyledHeadCell>
-                        {intl.informationType}
-                    </StyledHeadCell>
-                    <StyledHeadCell>
-                        {intl.policy}
-                    </StyledHeadCell>
-                    <StyledHeadCell>
-                        {intl.process}
-                    </StyledHeadCell>
-                    <StyledHeadCell>
-                        {intl.disclosure}
-                    </StyledHeadCell>
+                    {informationTypes && <StyledHeadCell> {intl.informationType} </StyledHeadCell>}
+                    {processes && <StyledHeadCell> {intl.process} </StyledHeadCell>}
+                    {policies && <StyledHeadCell> {intl.policy} </StyledHeadCell>}
+                    {disclosures && <StyledHeadCell> {intl.disclosure} </StyledHeadCell>}
                 </StyledHead>
                 <StyledBody>
                     {Array.from(Array(maxRows).keys()).map(index => {
@@ -37,18 +33,18 @@ export const Usage = (props: { usage: CodeUsage }) => {
                         const di = usage.disclosures[index];
                         return (
                             <StyledRow key={index}>
-                                <StyledCell>
+                                {informationTypes && <StyledCell>
                                     {it && <ObjectLink id={it.id} type={ObjectType.INFORMATION_TYPE} withHistory={true}>{it.name}</ObjectLink>}
-                                </StyledCell>
-                                <StyledCell>
-                                    {po && <ObjectLink id={po.id} type={ObjectType.POLICY} withHistory={true}>{po.name}</ObjectLink>}
-                                </StyledCell>
-                                <StyledCell>
+                                </StyledCell>}
+                                {processes && <StyledCell>
                                     {pr && <ObjectLink id={pr.id} type={ObjectType.PROCESS} withHistory={true}>{pr.name}</ObjectLink>}
-                                </StyledCell>
-                                <StyledCell>
+                                </StyledCell>}
+                                {policies && <StyledCell>
+                                    {po && <ObjectLink id={po.id} type={ObjectType.POLICY} withHistory={true}>{po.name}</ObjectLink>}
+                                </StyledCell>}
+                                {disclosures && <StyledCell>
                                     {di && <ObjectLink id={di.id} type={ObjectType.DISCLOSURE} withHistory={true}>{di.name}</ObjectLink>}
-                                </StyledCell>
+                                </StyledCell>}
                             </StyledRow>
                         )
                     })}
