41ae33aeccde334480d707bb451ff30abc113f54
Andreas Skomedal <andreas.skomedal@nav.no>
2020-01-14 15:00:06 +0100

    small team update

--- apps/frontend/src/components/common/Team.tsx
+++ apps/frontend/src/components/common/Team.tsx
@@ -7,7 +7,7 @@ import { faEnvelope, faTimesCircle, faUser, faUsers } from "@fortawesome/free-so
 import { copyToClipboard, intl, theme } from "../../util"
 import { StatefulTooltip } from "baseui/tooltip"
 import { Card, StyledBody } from "baseui/card"
-import { ListItem, ListItemLabel } from "baseui/list"
+import { ListItem, ListItemLabel, OverridesT } from "baseui/list"
 import { Button } from "baseui/button"
 
 
@@ -18,12 +18,14 @@ export const TeamPopover = (props: { teamId: string }) => {
     const [error, setError] = useState(false)
 
     useEffect(() => {
+        if (team.id !== props.teamId) {
+            setError(false)
+            setTeam(defaultTeam(props.teamId))
+        }
         (async () => {
             try {
-                setTeam(defaultTeam(props.teamId))
                 const team = await getTeam(props.teamId)
                 setTeam(team)
-                setError(false)
             } catch (e) {
                 console.error(e)
                 setError(true)
@@ -34,14 +36,23 @@ export const TeamPopover = (props: { teamId: string }) => {
     const errorIcon = <FontAwesomeIcon icon={faTimesCircle} color={theme.colors.negative500}/>
     const copyEmail = (member: Member) => () => copyToClipboard(member.email)
 
+    const listOverrides = {Content: {style: ({$theme}) => ({height: $theme.sizing.scale1000})}} as OverridesT
+
     return (
         <>
             {!error &&
-            <StatefulPopover content={() => (
-                <Card title={team.name}>
+            <StatefulPopover content={() =>
+                <Card title={team.name} overrides={{
+                    Body: {
+                        style: () => ({
+                            maxHeight: "600px",
+                            overflowY: "scroll"
+                        })
+                    }
+                }}>
                     <StyledBody>
                         {team.members.map((member, index) =>
-                            <ListItem key={index} endEnhancer={() =>
+                            <ListItem key={index} overrides={listOverrides} endEnhancer={() =>
                                 <StatefulTooltip content={`${intl.copied} ${member.email}`} triggerType="click">
                                     <Button size="compact" shape="pill" $style={{marginLeft: "1rem"}} onClick={copyEmail(member)}>
                                         <FontAwesomeIcon icon={faEnvelope} size="sm" style={{marginRight: ".5rem"}}/> {intl.email}
@@ -52,8 +63,7 @@ export const TeamPopover = (props: { teamId: string }) => {
                             </ListItem>
                         )}
                     </StyledBody>
-                </Card>
-            )}>
+                </Card>}>
               <Button size="compact" shape="pill" $style={{paddingTop: theme.sizing.scale100, paddingBottom: theme.sizing.scale100}}>
                 <FontAwesomeIcon icon={faUsers} size="sm" style={{marginRight: ".5rem"}}/> {team.name}
               </Button>
