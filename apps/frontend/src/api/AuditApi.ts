d4bd63c1d2a03393b49ebcc0df2f1f5620da7c55
Andreas Skomedal <andreas.skomedal@nav.no>
2020-01-20 16:55:19 +0100

    Filter audit by table

--- apps/frontend/src/api/AuditApi.ts
+++ apps/frontend/src/api/AuditApi.ts
@@ -1,16 +1,16 @@
 import axios from "axios"
-import { AuditItem, AuditLog, PageResponse } from "../constants"
+import { AuditItem, AuditLog, ObjectType, PageResponse } from "../constants"
 import moment from "moment";
 
 const server_polly = process.env.REACT_APP_POLLY_ENDPOINT;
 
 
 export const getAuditLog = async (id: string) => {
-    const auditLog = (await axios.get<AuditLog>(`${server_polly}/audit/log/${id}`)).data
-    auditLog.audits.sort((a, b) => moment(b.time).valueOf() - moment(a.time).valueOf())
-    return auditLog
+  const auditLog = (await axios.get<AuditLog>(`${server_polly}/audit/log/${id}`)).data
+  auditLog.audits.sort((a, b) => moment(b.time).valueOf() - moment(a.time).valueOf())
+  return auditLog
 }
 
-export const getAudits = async (page: number, count: number) => {
-    return (await axios.get<PageResponse<AuditItem>>(`${server_polly}/audit/?pageNumber=${page}&pageSize=${count}`)).data
+export const getAudits = async (page: number, count: number, table?: ObjectType) => {
+  return (await axios.get<PageResponse<AuditItem>>(`${server_polly}/audit/?pageNumber=${page}&pageSize=${count}` + (table ? `&table=${table}` : ''))).data
 }
--- apps/frontend/src/components/audit/AuditRecentTable.tsx
+++ apps/frontend/src/components/audit/AuditRecentTable.tsx
@@ -1,7 +1,7 @@
-import { Label1 } from "baseui/typography"
+import { Label1, Label3 } from "baseui/typography"
 import React, { useEffect, useState } from "react"
 import { getAudits } from "../../api/AuditApi"
-import { AuditItem, PageResponse } from "../../constants"
+import { AuditItem, ObjectType, PageResponse } from "../../constants"
 import { StyledBody, StyledCell, StyledHead, StyledHeadCell, StyledRow, StyledTable } from "baseui/table"
 import { intl } from "../../util"
 import moment from "moment"
@@ -19,118 +19,125 @@ import { faCode } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { AuditActionIcon } from "./AuditComponents"
 import randomColor from "randomcolor"
+import { StatefulSelect } from "baseui/select"
 
 export const AuditRecentTable = (props: { show: boolean }) => {
-    const [audits, setAudits] = useState<PageResponse<AuditItem>>({content: [], numberOfElements: 0, pageNumber: 0, pages: 0, pageSize: 1, totalElements: 0})
-    const [page, setPage] = React.useState(1);
-    const [limit, setLimit] = React.useState(20);
+  const [audits, setAudits] = useState<PageResponse<AuditItem>>({content: [], numberOfElements: 0, pageNumber: 0, pages: 0, pageSize: 1, totalElements: 0})
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(20);
+  const [table, setTable] = useState<ObjectType | undefined>(undefined)
 
-    const colors = _.uniq(audits.content.map(a => a.tableId))
-    .reduce((val, id) => {
-        val[id] = randomColor({seed: id, luminosity: "dark"})
-        return val
-    }, {} as { [id: string]: string })
+  const colors = _.uniq(audits.content.map(a => a.tableId))
+  .reduce((val, id) => {
+    val[id] = randomColor({seed: id, luminosity: "dark"})
+    return val
+  }, {} as { [id: string]: string })
 
-    useEffect(() => {
-        (async () => {
-            props.show && setAudits((await getAudits(page - 1, limit)))
-        })()
-    }, [page, limit, props.show])
+  useEffect(() => {
+    (async () => {
+      props.show && setAudits((await getAudits(page - 1, limit, table)))
+    })()
+  }, [page, limit, props.show, table])
 
-    const handlePageChange = (nextPage: number) => {
-        if (nextPage < 1) {
-            return;
-        }
-        if (nextPage > audits.pages) {
-            return;
-        }
-        setPage(nextPage);
-    };
-
-    const handleLimitChange = (nextLimit: number) => {
-        const nextPageNum = Math.ceil(audits.totalElements / nextLimit);
-        if (nextPageNum < page) {
-            setPage(nextPageNum);
-        }
-        setLimit(nextLimit);
-    };
-
-    if (!props.show) {
-        return null
+  const handlePageChange = (nextPage: number) => {
+    if (nextPage < 1) {
+      return;
     }
+    if (nextPage > audits.pages) {
+      return;
+    }
+    setPage(nextPage);
+  };
 
-    return (
-        <>
-            <Label1 marginBottom="1rem">{intl.lastChanges}</Label1>
-            <StyledTable>
-                <StyledHead>
-                    <StyledHeadCell $style={{maxWidth: "13%"}}>{intl.time}</StyledHeadCell>
-                    <StyledHeadCell $style={{maxWidth: "17%"}}>{intl.action}</StyledHeadCell>
-                    <StyledHeadCell>{intl.id}</StyledHeadCell>
-                    <StyledHeadCell>{intl.user}</StyledHeadCell>
-                </StyledHead>
-                <StyledBody>
-                    {audits.content.map((audit, index) => {
-                        const length = window.innerWidth > 1000 ? window.innerWidth > 1200 ? 40 : 30 : 20
-                        return (
-                            <StyledRow key={audit.id}>
-                                <Block position="absolute" marginLeft="-40px" display="block">
-                                    {audits.pageNumber * audits.pageSize + index + 1}
-                                </Block>
-                                <StyledCell $style={{maxWidth: "13%"}}>
-                                    <AuditButton kind="tertiary" id={audit.tableId} auditId={audit.id}>
-                                        <StatefulTooltip content={audit.time} placement={PLACEMENT.top}>{moment(audit.time).fromNow()}</StatefulTooltip>
-                                    </AuditButton>
-                                </StyledCell>
-                                <StyledCell $style={{maxWidth: "17%"}}>
-                                    <AuditActionIcon action={audit.action}/> {audit.table}
-                                </StyledCell>
-                                <StyledCell>
-                                    <StatefulTooltip content={audit.tableId} placement={PLACEMENT.top}>
-                                        <Block color={colors[audit.tableId]}>{_.truncate(audit.tableId, {length})}</Block>
-                                    </StatefulTooltip>
-                                </StyledCell>
-                                <StyledCell $style={{display: "flex", justifyContent: "space-between"}}>
-                                    <Block>{audit.user}</Block>
-                                    <Block>
-                                        <StatefulPopover content={() => (<ReactJson src={audit.data} name={null}/>)}>
-                                            <Button size="compact" shape="pill"><FontAwesomeIcon icon={faCode}/></Button>
-                                        </StatefulPopover>
-                                    </Block>
-                                </StyledCell>
-                            </StyledRow>
-                        )
-                    })}
-                </StyledBody>
-            </StyledTable>
+  useEffect(() => {
+    const nextPageNum = Math.ceil(audits.totalElements / limit);
+    if (audits.totalElements && nextPageNum < page) {
+      setPage(nextPageNum);
+    }
+  }, [limit, audits.totalElements])
 
-            <Block display="flex" justifyContent="space-between" marginTop="1rem">
-                <StatefulPopover
-                    content={({close}) => (
-                        <StatefulMenu
-                            items={[5, 10, 20, 50, 100].map(i => ({label: i,}))}
-                            onItemSelect={({item}) => {
-                                handleLimitChange(item.label);
-                                close();
-                            }}
-                            overrides={{
-                                List: {
-                                    style: {height: '150px', width: '100px'},
-                                },
-                            }}
-                        />
-                    )}
-                    placement={PLACEMENT.bottom}
-                >
-                    <Button kind={KIND.tertiary} endEnhancer={TriangleDown}>{`${limit} ${intl.rows}`}</Button>
-                </StatefulPopover>
-                <Pagination
-                    currentPage={page}
-                    numPages={audits.pages}
-                    onPageChange={({nextPage}) => handlePageChange(nextPage)}
-                    labels={{nextButton: intl.nextButton, prevButton: intl.prevButton}}
-                />
-            </Block>
-        </>
-    )
+  if (!props.show) {
+    return null
+  }
+
+  return (
+    <>
+      <Block display="flex" justifyContent="space-between" marginBottom=".5rem">
+        <Label1>{intl.lastChanges}</Label1>
+        <Block width="300px" display="flex" justifyContent="space-between">
+          <Label3 alignSelf="center" marginRight=".5rem">{intl.table}: </Label3>
+          <StatefulSelect size="compact" options={Object.keys(ObjectType).map(ot => ({id: ot, label: ot}))} onChange={p => setTable(p?.value[0]?.id as ObjectType)}/>
+        </Block>
+      </Block>
+      <StyledTable>
+        <StyledHead>
+          <StyledHeadCell $style={{maxWidth: "13%"}}>{intl.time}</StyledHeadCell>
+          <StyledHeadCell $style={{maxWidth: "17%"}}>{intl.action}</StyledHeadCell>
+          <StyledHeadCell>{intl.id}</StyledHeadCell>
+          <StyledHeadCell>{intl.user}</StyledHeadCell>
+        </StyledHead>
+        <StyledBody>
+          {audits.content.map((audit, index) => {
+            const length = window.innerWidth > 1000 ? window.innerWidth > 1200 ? 40 : 30 : 20
+            return (
+              <StyledRow key={audit.id}>
+                <Block position="absolute" marginLeft="-40px" display="block">
+                  {audits.pageNumber * audits.pageSize + index + 1}
+                </Block>
+                <StyledCell $style={{maxWidth: "13%"}}>
+                  <AuditButton kind="tertiary" id={audit.tableId} auditId={audit.id}>
+                    <StatefulTooltip content={audit.time} placement={PLACEMENT.top}>{moment(audit.time).fromNow()}</StatefulTooltip>
+                  </AuditButton>
+                </StyledCell>
+                <StyledCell $style={{maxWidth: "17%"}}>
+                  <AuditActionIcon action={audit.action}/> {audit.table}
+                </StyledCell>
+                <StyledCell>
+                  <StatefulTooltip content={audit.tableId} placement={PLACEMENT.top}>
+                    <Block color={colors[audit.tableId]}>{_.truncate(audit.tableId, {length})}</Block>
+                  </StatefulTooltip>
+                </StyledCell>
+                <StyledCell $style={{display: "flex", justifyContent: "space-between"}}>
+                  <Block>{audit.user}</Block>
+                  <Block>
+                    <StatefulPopover content={() => (<ReactJson src={audit.data} name={null}/>)}>
+                      <Button size="compact" shape="pill"><FontAwesomeIcon icon={faCode}/></Button>
+                    </StatefulPopover>
+                  </Block>
+                </StyledCell>
+              </StyledRow>
+            )
+          })}
+        </StyledBody>
+      </StyledTable>
+
+      <Block display="flex" justifyContent="space-between" marginTop="1rem">
+        <StatefulPopover
+          content={({close}) => (
+            <StatefulMenu
+              items={[5, 10, 20, 50, 100].map(i => ({label: i,}))}
+              onItemSelect={({item}) => {
+                setLimit(item.label);
+                close();
+              }}
+              overrides={{
+                List: {
+                  style: {height: '150px', width: '100px'},
+                },
+              }}
+            />
+          )}
+          placement={PLACEMENT.bottom}
+        >
+          <Button kind={KIND.tertiary} endEnhancer={TriangleDown}>{`${limit} ${intl.rows}`}</Button>
+        </StatefulPopover>
+        <Pagination
+          currentPage={page}
+          numPages={audits.pages}
+          onPageChange={({nextPage}) => handlePageChange(nextPage)}
+          labels={{nextButton: intl.nextButton, prevButton: intl.prevButton}}
+        />
+      </Block>
+    </>
+  )
 }
