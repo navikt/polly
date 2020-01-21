b6383990d14084e33020ab6fd6342236e14d4046
Andreas Skomedal <andreas.skomedal@nav.no>
2020-01-21 14:46:23 +0100

    fix import

--- apps/backend/polly-test/polly-test-integration/src/test/java/no/nav/data/polly/process/ProcessUpdateIT.java
+++ apps/backend/polly-test/polly-test-integration/src/test/java/no/nav/data/polly/process/ProcessUpdateIT.java
@@ -17,6 +17,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
