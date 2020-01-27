cf410125132c39cfe14b12d4ab41f220e3cc106e
Shayan Yazdanmehr <shayan.yazdanmehr@gmail.com>
2020-01-27 09:34:42 +0100

    #1: remove console loggers #2: manipulate Formik's arrayHelpers in order to modify categories

--- apps/frontend/src/components/document/component/FieldSubjectCategory.tsx
+++ apps/frontend/src/components/document/component/FieldSubjectCategory.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import {Select, Value} from "baseui/select";
 import {codelist, ListName} from "../../../service/Codelist";
-import {DocumentTableRow} from "../../../constants";
+import {DocumentInformationTypes, DocumentTableRow} from "../../../constants";
 import index from "../../Purpose/Accordion";
+import {FieldArrayRenderProps} from "formik";
 
-const FieldSubjectCategory = (props: { value?: string, rowData:DocumentTableRow, setRowData:Function, index:number }) => {
+const FieldSubjectCategory = (props: {
+  value?: string,
+  rowData:DocumentTableRow,
+  setRowData:Function,
+  index:number,
+  arrayHelpers:FieldArrayRenderProps}) => {
+
   const [value, setValue] = React.useState<Value>(props.value ? [{
     id: props.value,
     label: codelist.getShortname(ListName.SUBJECT_CATEGORY, props.value)
@@ -15,11 +22,15 @@ const FieldSubjectCategory = (props: { value?: string, rowData:DocumentTableRow,
       options={codelist.getParsedOptions(ListName.SUBJECT_CATEGORY)}
       onChange={({value}) => {
         setValue(value);
-        console.log(value);
-        console.log(props.rowData);
         let newRowData = props.rowData;
         newRowData.categories = [...value];
         props.setRowData(newRowData,index);
+
+        let  informationType = props.arrayHelpers.form.values.informationTypes[props.index] as DocumentInformationTypes;
+        // @ts-ignore
+        informationType.subjectCategories = [...value].map(category=>category.id);
+        props.arrayHelpers.replace(props.index,informationType);
+
       }}
       value={value}
       multi
