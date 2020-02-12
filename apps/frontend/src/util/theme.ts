b68b844f8e0d7ad8cdb6d2b4d0e46ff4845fe713
danishnav <54104284+danishnav@users.noreply.github.com>
2020-02-12 15:41:17 +0100

    starta med oppadtering av theming

--- apps/frontend/src/util/theme.ts
+++ apps/frontend/src/util/theme.ts
@@ -1,4 +1,15 @@
-import { createTheme, lightThemePrimitives } from "baseui"
+import {createTheme, lightThemePrimitives} from 'baseui';
+const primitives = {
+  ...lightThemePrimitives,
+  primary: '#6699ff', // hot pink
+  primary50: '#FDEDFC',
+  primary100: '#FCD3F9',
+  primary200: '#F89FF3',
+  primary300: '#F45AEA',
+  primary400: '#F127E4',
+  primary500: '#B71DAD',
+  primary600: '#901788',
+  primary700: '#600F5B',
+};
 
-export const theme = createTheme(lightThemePrimitives)
-
+export const theme = createTheme(primitives);
