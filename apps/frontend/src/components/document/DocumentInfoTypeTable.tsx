df71f9d59e2c43cf54038fc45acbca48e30a80a3
Andreas Skomedal <andreas.skomedal@nav.no>
2020-02-14 17:07:21 +0100

    document table, last public table replaced

--- apps/frontend/src/components/document/DocumentInfoTypeTable.tsx
+++ apps/frontend/src/components/document/DocumentInfoTypeTable.tsx
@@ -1,65 +1,46 @@
 import * as React from 'react'
 import { useTable } from '../../util/hooks'
 import { DocumentInfoTypeUse, documentSort } from '../../constants'
-import { useStyletron, withStyle } from 'styletron-react'
-import { StyledTable, SortableHeadCell, StyledHead, StyledBody, StyledRow, StyledCell } from 'baseui/table'
 import { intl } from '../../util'
 import RouteLink from '../common/RouteLink'
-
-const StyledHeader = withStyle(StyledHead, {
-    backgroundColor: "transparent",
-    boxShadow: "none",
-    borderBottom: "2px solid #E9E7E7"
-});
-
-const CustomStyledRow = withStyle(StyledRow, {
-    borderBottom: "1px solid #E9E7E7",
-    padding: "8px",
-    fontSize: "24px"
-});
+import { Cell, HeadCell, Row, Table } from "../common/Table"
 
 type DocumentInfoTypeTableProps = {
-    list: DocumentInfoTypeUse[]
+  list: DocumentInfoTypeUse[]
 }
 
 const DocumentInfoTypeTable = (props: DocumentInfoTypeTableProps) => {
-    const { list } = props
-    const [table, sortColumn] = useTable<DocumentInfoTypeUse, keyof DocumentInfoTypeUse>(list, { sorting: documentSort, initialSortColumn: "informationType" })
-    const [useCss] = useStyletron();
+  const {list} = props
+  const [table, sortColumn] = useTable<DocumentInfoTypeUse, keyof DocumentInfoTypeUse>(list, {sorting: documentSort, initialSortColumn: "informationType"})
 
-    return (
-        <StyledTable className={useCss({ overflow: "hidden !important" })}>
-            <StyledHeader>
-                <SortableHeadCell
-                    title={intl.informationType}
-                    direction={table.direction.informationType}
-                    onSort={() => sortColumn('informationType')}
-                    fillClickTarget
-                />
-                <SortableHeadCell
-                    title={intl.subjectCategories}
-                    direction={table.direction.subjectCategories}
-                    onSort={() => sortColumn('subjectCategories')}
-                    fillClickTarget
-                />
-            </StyledHeader>
-
-            <StyledBody>
-                {table.data.map((row, index) => (
-                    <CustomStyledRow>
-                        <StyledCell>
-                            <RouteLink href={`/informationtype/${row.informationType.id}`}>{row.informationType.name}</RouteLink>
-                        </StyledCell>
-                        <StyledCell>
-                            {row.subjectCategories && row.subjectCategories.map(sc => sc.shortName).join(', ')}
-                        </StyledCell>
-                    </CustomStyledRow>
-                ))}
-
-
-            </StyledBody>
-        </StyledTable>
-    )
+  return (
+    <Table
+      headers={
+        <>
+          <HeadCell
+            title={intl.informationType}
+            column='informationType'
+            tableState={[table, sortColumn]}
+          />
+          <HeadCell
+            title={intl.subjectCategories}
+            column='subjectCategories'
+            tableState={[table, sortColumn]}
+          />
+        </>
+      }>
+      {table.data.map((row, index) => (
+        <Row key={index}>
+          <Cell>
+            <RouteLink href={`/informationtype/${row.informationType.id}`}>{row.informationType.name}</RouteLink>
+          </Cell>
+          <Cell>
+            {row.subjectCategories && row.subjectCategories.map(sc => sc.shortName).join(', ')}
+          </Cell>
+        </Row>
+      ))}
+    </Table>
+  )
 }
 
 export default DocumentInfoTypeTable
