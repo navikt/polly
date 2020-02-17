93175860e6f1f575469f970da97b2e8c86714a26
Andreas Skomedal <andreas.skomedal@nav.no>
2020-02-17 14:15:09 +0100

    Change main colors

--- apps/frontend/src/util/theme.ts
+++ apps/frontend/src/util/theme.ts
@@ -6,28 +6,19 @@ const primitives: ThemePrimitives = {
   ...lightThemePrimitives,
   primary: "#19548a",
   primary50: "#eaf4fc",
-  primary100: colors.blue100,
+  primary100: "#B5CCDF",
   primary200: "#99c2e8",
-  primary300: colors.blue300,
+  primary300: "#396FA1",
   primary400: "#19548a",
   primary500: colors.blue500,
   primary600: colors.blue600,
   primary700: colors.blue700,
-
-  accent: colors.platinum700,
-  accent50: colors.platinum50,
-  accent100: colors.platinum100,
-  accent200: colors.platinum200,
-  accent300: colors.platinum300,
-  accent400: colors.platinum400,
-  accent500: colors.platinum500,
-  accent600: colors.platinum600,
-  accent700: colors.platinum700,
 }
 
 const overrides = {
   colors: {
-    buttonSecondaryText: primitives.accent600,
+    buttonSecondaryFill: primitives.primary50,
+    // buttonSecondaryText: primitives.mono900,
 
     linkVisited: primitives.primary400,
   },
