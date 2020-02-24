a8cf1c797c7b284a8e209cf6fb388fb668ff4219
Shayan Yazdanmehr <shayan.yazdanmehr@nav.no>
2020-02-24 14:13:37 +0100

    Delete blue color underline from links in the sidebar

--- apps/frontend/src/components/SideBar/index.tsx
+++ apps/frontend/src/components/SideBar/index.tsx
@@ -2,13 +2,13 @@ import * as React from 'react'
 import {intl, theme} from '../../util';
 import {Block, BlockProps} from 'baseui/block';
 import {H6, Paragraph2, Paragraph4} from 'baseui/typography';
-import RouteLink from '../common/RouteLink';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faChevronDown, faChevronRight} from '@fortawesome/free-solid-svg-icons';
 import {features} from "../../util/feature-toggle"
 import NavLogo from '../../resources/navlogo.svg'
 import Slackogo from '../../resources/Slack_Monochrome_White.svg'
 import {useLocation} from 'react-router-dom'
+import {StyledLink} from "baseui/link";
 
 const sideBarProps: BlockProps = {
   position: 'fixed',
@@ -25,7 +25,7 @@ const items: BlockProps = {
 const NavItem = (props: { text: string, to: string, active?: boolean }) => {
   return (
     <React.Fragment>
-      <RouteLink href={props.to}>
+      <StyledLink style={{textDecoration:"none"}} href={props.to}>
         <Block display="flex" alignItems="center">
           <Block marginRight="scale400">
             <FontAwesomeIcon
@@ -35,24 +35,23 @@ const NavItem = (props: { text: string, to: string, active?: boolean }) => {
           </Block>
           <Paragraph2 color="white">{props.text}</Paragraph2>
         </Block>
-      </RouteLink>
+      </StyledLink>
     </React.Fragment>
   )
 }
 
 const Brand = () => (
-  <RouteLink href="/">
+  <StyledLink style={{textDecoration:"none"}} href="/">
     <Block display="flex" alignItems="center" padding="1rem" marginTop="1rem">
       <H6 color="white" marginTop="0" marginLeft="5px" marginBottom="2rem">Behandlingskatalog</H6>
     </Block>
-  </RouteLink>
+  </StyledLink>
 )
 
 const SideBar = () => {
   return (
     <Block {...sideBarProps}>
-      <Brand/>
-
+        <Brand/>
       <Block {...items}>
         <NavItem to="/purpose" text={intl.processingActivities}/>
         <NavItem to="/informationtype" text={intl.informationTypes}/>
