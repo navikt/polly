04890550eea63e17e29129602d1095f37b6e6496
Andreas Skomedal <andreas.skomedal@nav.no>
2020-02-27 13:08:50 +0100

    local consumer-id

--- apps/frontend/src/setupProxy.js
+++ apps/frontend/src/setupProxy.js
@@ -2,18 +2,26 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // Used in local development server only
 module.exports = function (app) {
+
   const target = 'http://localhost:8080'
+  const headers = {
+    'Nav-Consumer-Id': 'behandlingskatalog-local'
+  }
+
   app.use(
     '/api',
     createProxyMiddleware({
-      target: target,
       pathRewrite: {
         '^/api': ''
-      }
+      },
+      target,
+      headers
     })
   );
+
   app.use(
     '/login',
-    createProxyMiddleware({ target })
-  )
+    createProxyMiddleware({ target, headers })
+  );
+
 };
