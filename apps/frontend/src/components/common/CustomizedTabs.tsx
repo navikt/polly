28ce998ea7d8e0e4176b56d07436330e4b88adc2
Shayan Yazdanmehr <shayan.yazdanmehr@nav.no>
2020-02-28 10:07:28 +0100

    delete unused context from the component

--- apps/frontend/src/components/common/CustomizedTabs.tsx
+++ apps/frontend/src/components/common/CustomizedTabs.tsx
@@ -3,32 +3,28 @@ import {Tabs, TabsProps} from "baseui/tabs";
 import {theme} from "../../util";
 import {paddingAll, paddingZero} from "./Style";
 
-const TabsContext = React.createContext<Partial<TabsProps>>({});
-
 export const CustomizedTabs = (props: TabsProps) => {
-  return(
-    <TabsContext.Provider value={props}>
-      <Tabs
-        onChange={props.onChange}
-        activeKey={props.activeKey}
-        overrides={{
-          Root: {
-            style: {
-              outline: `4px ${theme.colors.primary200} solid`
-            }
-          },
-          TabContent: {
-            style: paddingZero
-          },
-          TabBar: {
-            style: {
-              ...paddingAll(theme.sizing.scale600)
-            }
+  return (
+    <Tabs
+      onChange={props.onChange}
+      activeKey={props.activeKey}
+      overrides={{
+        Root: {
+          style: {
+            outline: `4px ${theme.colors.primary200} solid`
           }
-        }}
-      >
-        {props.children}
-      </Tabs>
-    </TabsContext.Provider>
+        },
+        TabContent: {
+          style: paddingZero
+        },
+        TabBar: {
+          style: {
+            ...paddingAll(theme.sizing.scale600)
+          }
+        }
+      }}
+    >
+      {props.children}
+    </Tabs>
   )
 }
