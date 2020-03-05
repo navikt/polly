e8318c3298daafbe209e2518be7c317ca7c38a10
Andreas Skomedal <andreas.skomedal@nav.no>
2020-03-05 11:34:55 +0100

    Lovdata forskrift

--- apps/frontend/src/components/common/LegalBasis.tsx
+++ apps/frontend/src/components/common/LegalBasis.tsx
@@ -1,37 +1,39 @@
-import * as React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faEdit, faExclamation, faTrash } from "@fortawesome/free-solid-svg-icons"
-import { ARTWORK_SIZES, ListItem, ListItemLabel } from "baseui/list";
+import * as React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faExclamation, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { ARTWORK_SIZES, ListItem, ListItemLabel } from 'baseui/list'
 import { Block } from 'baseui/block'
-import { Button } from "baseui/button"
+import { Button } from 'baseui/button'
 
-import { LegalBasis, LegalBasisFormValues, PolicyAlert } from "../../constants"
-import { codelist, ListName } from "../../service/Codelist"
-import { processString } from "../../util/string-processor"
-import { intl, theme } from "../../util"
-import { ActiveIndicator } from "./Durations"
-import { StyledLink } from "baseui/link"
-import { env } from "../../util/env"
+import { LegalBasis, LegalBasisFormValues, PolicyAlert } from '../../constants'
+import { codelist, ListName } from '../../service/Codelist'
+import { processString } from '../../util/string-processor'
+import { intl, theme } from '../../util'
+import { ActiveIndicator } from './Durations'
+import { StyledLink } from 'baseui/link'
+import { env } from '../../util/env'
 
 export const LegalBasisView = (props: { legalBasis?: LegalBasis, legalBasisForm?: LegalBasisFormValues }) => {
-  const input = props.legalBasis ? props.legalBasis : props.legalBasisForm;
-  if (!input) return null;
-  const {start, end, description} = input;
-  const gdpr = props.legalBasis ? props.legalBasis.gdpr.code : props.legalBasisForm!.gdpr;
-  const nationalLaw = props.legalBasis ? props.legalBasis?.nationalLaw?.code : props.legalBasisForm!.nationalLaw;
+  const input = props.legalBasis ? props.legalBasis : props.legalBasisForm
+  if (!input) return null
+  const {start, end, description} = input
+  const gdpr = props.legalBasis ? props.legalBasis.gdpr.code : props.legalBasisForm!.gdpr
+  const nationalLaw = props.legalBasis ? props.legalBasis?.nationalLaw?.code : props.legalBasisForm!.nationalLaw
 
-  let gdprDisplay = gdpr && codelist.getShortname(ListName.GDPR_ARTICLE, gdpr);
-  let nationalLawDisplay = nationalLaw && codelist.getShortname(ListName.NATIONAL_LAW, nationalLaw);
-  let nationalLawId = nationalLaw && codelist.valid(ListName.NATIONAL_LAW, nationalLaw) && codelist.getDescription(ListName.NATIONAL_LAW, nationalLaw);
+  let gdprDisplay = gdpr && codelist.getShortname(ListName.GDPR_ARTICLE, gdpr)
+  let nationalLawDisplay = nationalLaw && codelist.getShortname(ListName.NATIONAL_LAW, nationalLaw)
+  let nationalLawId = nationalLaw && codelist.valid(ListName.NATIONAL_LAW, nationalLaw) && codelist.getDescription(ListName.NATIONAL_LAW, nationalLaw)
 
-  let descriptionText = nationalLawId ? legalBasisLinkProcessor(nationalLawId, description) : description;
+  let descriptionText = nationalLawId ? legalBasisLinkProcessor(nationalLawId, description) : description
 
   return (
     <span>
       <ActiveIndicator start={start} end={end}/> {gdprDisplay}{(nationalLawDisplay || descriptionText) && ', '} {nationalLawDisplay} {descriptionText}
     </span>
   )
-};
+}
+
+const lovdataBase = (nationalLaw: string) => (codelist.isForskrift(nationalLaw) ? env.lovdataForskriftBaseUrl : env.lovdataLovBaseUrl) + codelist.getDescription(ListName.NATIONAL_LAW, nationalLaw)
 
 const legalBasisLinkProcessor = (law: string, text?: string) => processString([
   {
@@ -42,17 +44,17 @@ const legalBasisLinkProcessor = (law: string, text?: string) => processString([
     // tripe '§§§' is hidden, used as a trick in combination with rule 1 above
     regex: /§(§§)?(§)?\s*(\d+(-\d+)?)/g,
     fn: (key: string, result: string[]) =>
-      <StyledLink key={key} href={`${env.lovdataBaseUrl + codelist.getDescription(ListName.NATIONAL_LAW, law)}/§${result[3]}`} target="_blank" rel="noopener noreferrer">
-        {(!result[1] && !result[2]) && `§`} {result[2] && `§§`} {result[3]}
+      <StyledLink key={key} href={`${lovdataBase(law)}/§${result[3]}`} target="_blank" rel="noopener noreferrer">
+        {(!result[1] && !result[2]) && '§'} {result[2] && '§§'} {result[3]}
       </StyledLink>
   }, {
     regex: /kap(ittel)?\s*(\d+)/gi,
     fn: (key: string, result: string[]) =>
-      <StyledLink key={key} href={`${env.lovdataBaseUrl + codelist.getDescription(ListName.NATIONAL_LAW, law)}/KAPITTEL_${result[2]}`} target="_blank" rel="noopener noreferrer">
+      <StyledLink key={key} href={`${lovdataBase(law)}/KAPITTEL_${result[2]}`} target="_blank" rel="noopener noreferrer">
         Kapittel {result[2]}
       </StyledLink>
   }
-])(text);
+])(text)
 
 export const LegalBasesNotClarified = (props: { alert?: PolicyAlert }) => {
   const color = theme.colors.warning500
@@ -70,7 +72,7 @@ export const LegalBasesNotClarified = (props: { alert?: PolicyAlert }) => {
       </Block>
     </Block>
   )
-};
+}
 
 export const ListLegalBases = (
   props: {
@@ -78,8 +80,8 @@ export const ListLegalBases = (
     onRemove: (index: number) => void,
     onEdit: (index: number) => void
   },) => {
-  const {legalBases, onRemove, onEdit} = props;
-  if (!legalBases) return null;
+  const {legalBases, onRemove, onEdit} = props
+  if (!legalBases) return null
   return (
     <React.Fragment>
       {legalBases.map((legalBasis: LegalBasisFormValues, i: number) => (
@@ -93,7 +95,7 @@ export const ListLegalBases = (
                   kind="minimal"
                   size="compact"
                   onClick={() => {
-                    onEdit(i);
+                    onEdit(i)
                   }}
                 >
                   <FontAwesomeIcon icon={faEdit}/>
@@ -118,13 +120,13 @@ export const ListLegalBases = (
       ))}
     </React.Fragment>
   )
-};
+}
 
 export const ListLegalBasesInTable = (props: { legalBases: LegalBasis[] }) => {
-  const {legalBases} = props;
+  const {legalBases} = props
   return (
     <Block>
-      <ul style={{listStyle: "none", paddingInlineStart: 0, marginTop: 0, marginBottom: 0}}>
+      <ul style={{listStyle: 'none', paddingInlineStart: 0, marginTop: 0, marginBottom: 0}}>
         {legalBases.map((legalBasis, i) => (
           <Block marginBottom="8px" key={i}>
             <li><LegalBasisView legalBasis={legalBasis}/></li>
@@ -133,4 +135,4 @@ export const ListLegalBasesInTable = (props: { legalBases: LegalBasis[] }) => {
       </ul>
     </Block>
   )
-};
+}
