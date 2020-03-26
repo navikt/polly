27315ad3043b40e30db9b5a702d47f45252ab502
Shayan Yazdanmehr <shayan.yazdanmehr@nav.no>
2020-03-26 14:20:30 +0100

    Remove extra search bar from the third party page

--- apps/frontend/src/pages/ThirdPartySearchPage.tsx
+++ apps/frontend/src/pages/ThirdPartySearchPage.tsx
@@ -2,49 +2,23 @@ import * as React from "react";
 import {intl, useAwait} from "../util"
 import {RouteComponentProps} from "react-router-dom";
 import {codelist, ListName} from "../service/Codelist";
-import {Block, BlockProps} from "baseui/block";
-import {StatefulSelect} from "baseui/select";
 import {user} from "../service/User";
-import {useStyletron} from "styletron-react";
 import {H4} from "baseui/typography";
 import {StyledSpinnerNext} from "baseui/spinner"
 import AlphabeticList from "../components/common/AlphabeticList";
 
-const rowBlockProps: BlockProps = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  marginBottom: '3rem',
-}
-
 const ThirdPartySearchPage = (props: RouteComponentProps) => {
-  const [css] = useStyletron();
   const [isLoading, setIsLoading] = React.useState<boolean>(true)
   useAwait(user.wait())
   useAwait(codelist.wait(), setIsLoading)
 
   const thirdPartyList = codelist.getCodes(ListName.THIRD_PARTY)
 
-  const handleChangeThirdParty = async (thirdParty?: string) => {
-    if (thirdParty) {
-      props.history.push(`/thirdparty/${thirdParty}`)
-    }
-  }
-
   return (
     <React.Fragment>
       <H4>{intl.thirdParties}</H4>
 
       {isLoading && <StyledSpinnerNext/>}
-      {!isLoading && (
-        <Block {...rowBlockProps}>
-          <StatefulSelect
-            options={codelist.getParsedOptions(ListName.THIRD_PARTY)}
-            placeholder={intl.disclosureSelect}
-            maxDropdownHeight="350px"
-            onChange={(event) => handleChangeThirdParty((event.option?.id) as string | undefined)}
-          />
-        </Block>
-      )}
 
       {!!thirdPartyList.length && (
         <AlphabeticList listName={ListName.THIRD_PARTY} baseUrl={"/thirdparty/"}/>
