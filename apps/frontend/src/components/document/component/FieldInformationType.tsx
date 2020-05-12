dd1012c202e7fa72576c277ae06f5d13addb073d
Shayan Yazdanmehr <shayan.yazdanmehr@nav.no>
2020-05-12 13:48:16 +0200

    design bug fix (remove placeholder after selecting an information type)

--- apps/frontend/src/components/document/component/FieldInformationType.tsx
+++ apps/frontend/src/components/document/component/FieldInformationType.tsx
@@ -1,15 +1,15 @@
-import { DocumentInfoTypeUse, InformationTypeShort } from '../../../constants'
-import { Select, TYPE } from 'baseui/select'
+import {DocumentInfoTypeUse, InformationTypeShort} from '../../../constants'
+import {Select, TYPE} from 'baseui/select'
 import React from 'react'
-import { useInfoTypeSearch } from '../../../api'
-import { intl } from '../../../util'
-import { PLACEMENT } from 'baseui/popover'
+import {useInfoTypeSearch} from '../../../api'
+import {intl} from '../../../util'
+import {PLACEMENT} from 'baseui/popover'
 
 const FieldInformationType = (props: {
   documentInformationType: DocumentInfoTypeUse,
   handleChange: Function
 }) => {
-  const { documentInformationType, handleChange } = props
+  const {documentInformationType, handleChange} = props
   const [searchKeyword, setSearchKeyword, isLoading] = useInfoTypeSearch()
 
   const [selectedInformationType, setSelectedInformationType] = React.useState<InformationTypeShort>()
@@ -26,12 +26,12 @@ const FieldInformationType = (props: {
       searchable={true}
       type={TYPE.search}
       options={searchKeyword}
-      placeholder={intl.informationTypeSearch}
+      placeholder={selectedInformationType ? "" : intl.informationTypeSearch}
       overrides={{
-        Popover:{
-          props:{
-            Body:{
-              placement:PLACEMENT.bottom,
+        Popover: {
+          props: {
+            Body: {
+              placement: PLACEMENT.bottom,
             }
           }
         }
@@ -39,8 +39,8 @@ const FieldInformationType = (props: {
       value={selectedInformationType as any}
       onInputChange={event => setSearchKeyword(event.currentTarget.value)}
       onChange={(params) => {
-          setSelectedInformationType(params.value[0] as InformationTypeShort)
-          handleChange({...documentInformationType, informationTypeId: !params.value[0] ? '' : params.value[0].id})
+        setSelectedInformationType(params.value[0] as InformationTypeShort)
+        handleChange({...documentInformationType, informationTypeId: !params.value[0] ? '' : params.value[0].id})
       }}
       filterOptions={options => options}
       labelKey="name"
