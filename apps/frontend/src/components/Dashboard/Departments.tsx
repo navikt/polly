ed961afbf7e520e9bb48b19b2b118867d92e64ac
danishnav <danish.khan@nav.no>
2020-06-10 16:38:07 +0200

    Added tooltip and using changed names on department card

--- apps/frontend/src/components/Dashboard/Departments.tsx
+++ apps/frontend/src/components/Dashboard/Departments.tsx
@@ -3,10 +3,12 @@ import { Card } from 'baseui/card'
 import { cardShadow } from '../common/Style'
 import { Block } from 'baseui/block'
 import { Paragraph2, Label1 } from 'baseui/typography'
-import { theme } from '../../util'
+import { theme, useAwait } from '../../util'
 import { DashboardData, DepartmentProcess } from '../../constants'
 import { StyledLink } from 'baseui/link'
 import { useStyletron } from 'styletron-react'
+import { codelist, ListName } from '../../service/Codelist'
+import { StatefulTooltip, PLACEMENT } from 'baseui/tooltip'
 
 const TextWithNumber = (props: { label: string; number: number }) => (
     <Block display="flex" width="100%" marginBottom="0" justifyContent="center">
@@ -15,29 +17,10 @@ const TextWithNumber = (props: { label: string; number: number }) => (
 )
 
 const parsedDepartmentName = (department: string) => {
-    let parsedValue
-    switch (department) {
-        case 'KOMMUNIKASJON':
-            parsedValue = 'Kommunikasjon'
-            break;
-        case 'KUNNSKAP':
-            parsedValue = 'Kunnskap'
-            break;
-        case 'HRAVD':
-            parsedValue = 'HR'
-            break;
-        case 'ITAVD':
-            parsedValue = 'IT'
-            break;
-        case 'OESA':
-            parsedValue = 'ØSA'
-            break;
-        default:
-            parsedValue = department
-            break;
-    }
-    return parsedValue
+    if (department === 'OESA') return 'ØSA'
+    return codelist.getCode(ListName.DEPARTMENT, department)?.code as string
 }
+
 
 type DepartmentCardProps = {
     department: DepartmentProcess;
@@ -48,23 +31,24 @@ const DepartmentCard = (props: DepartmentCardProps) => {
     const { department } = props
 
     return (
-        <StyledLink href={"/process/department/" + department.department} className={linkCss}>
-            <Card overrides={cardShadow}>
-                <Block
-                    display='flex'
-                    flexDirection='column'
-                    alignItems='center'
-                    justifyContent='space-around'
-                    width="130px"
-                    height="130px"
-                >
-                    <Label1 color={theme.colors.accent300} $style={{ textAlign: 'center' }}>{parsedDepartmentName(department.department)}</Label1>
-                    <TextWithNumber label="Fullførte" number={department.processesCompleted} />
-                    <TextWithNumber label="Under arbeid" number={department.processesInProgress} />
-                </Block>
-            </Card>
-        </StyledLink>
-
+        <StatefulTooltip content={codelist.getCode(ListName.DEPARTMENT, department.department)?.shortName} placement={PLACEMENT.topLeft}>
+            <StyledLink href={"/process/department/" + department.department} className={linkCss}>
+                <Card overrides={cardShadow}>
+                    <Block
+                        display='flex'
+                        flexDirection='column'
+                        alignItems='center'
+                        justifyContent='space-around'
+                        width="130px"
+                        height="130px"
+                    >
+                        <Label1 color={theme.colors.accent300} $style={{ textAlign: 'center' }}>{parsedDepartmentName(department.department)}</Label1>
+                        <TextWithNumber label="Fullførte" number={department.processesCompleted} />
+                        <TextWithNumber label="Under arbeid" number={department.processesInProgress} />
+                    </Block>
+                </Card>
+            </StyledLink>
+        </StatefulTooltip>
     )
 }
 
@@ -73,8 +57,10 @@ type DepartmentsProps = {
 }
 const Departments = (props: DepartmentsProps) => {
     const { data } = props
+    useAwait(codelist.wait())
 
     const sortedData = () => data.departmentProcesses.sort((a, b) => parsedDepartmentName(a.department).localeCompare(b.department))
+
     return (
         <Block width="100%" display="flex" flexWrap >
             {sortedData().map((department: DepartmentProcess) => (
