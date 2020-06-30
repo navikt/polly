3bdd4ec88b78fe0fdbf8d10b19e03035cbbe4aa6
danishnav <danish.khan@nav.no>
2020-06-30 08:19:35 +0200

    test commit

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -1,17 +1,17 @@
-import {intl, theme} from '../util'
+import { intl, theme } from '../util'
 import * as React from 'react'
-import {useEffect, useState} from 'react'
-import {Block} from 'baseui/block'
-import {DashboardData, ProcessField, ProcessState, Settings} from '../constants'
-import {getSettings} from '../api/SettingsApi'
+import { useEffect, useState } from 'react'
+import { Block } from 'baseui/block'
+import { DashboardData, ProcessField, ProcessState, Settings } from '../constants'
+import { getSettings } from '../api/SettingsApi'
 import ReactMarkdown from 'react-markdown/with-html'
-import {Card} from 'baseui/card'
-import {cardShadow} from '../components/common/Style'
+import { Card } from 'baseui/card'
+import { cardShadow } from '../components/common/Style'
 import Departments from '../components/Dashboard/Departments'
-import {getDashboard} from '../api'
-import {Chart} from '../components/Dashboard/Chart'
-import {RouteComponentProps} from 'react-router-dom'
-import {LastEvents} from '../components/audit/LastEvents'
+import { getDashboard } from '../api'
+import { Chart } from '../components/Dashboard/Chart'
+import { RouteComponentProps } from 'react-router-dom'
+import { LastEvents } from '../components/audit/LastEvents'
 
 export const Main = (props: RouteComponentProps) => {
   const [settings, setSettings] = useState<Settings>()
@@ -38,120 +38,120 @@ export const Main = (props: RouteComponentProps) => {
       {
         !isLoading && dashData && (
           <>
-            <Departments data={dashData}/>
+            <Departments data={dashData} />
 
             <Block display='flex' flexWrap={true} width={'100%'}>
               <Block marginTop={theme.sizing.scale600} marginRight={theme.sizing.scale600} minWidth={'520px'}>
                 <Chart title={intl.dpiaNeeded} size={chartSize}
-                       data={
-                         [
-                           {
-                             label: `${intl.yes}`,
-                             color: colorYes,
-                             size: dashData.allProcesses.dpia.yes,
-                             onClick: () => clickOnPieChartSlice(ProcessField.DPIA, ProcessState.YES)
-                           },
-                           {
-                             label: `${intl.no}`,
-                             color: colorNo,
-                             size: dashData.allProcesses.dpia.no,
-                             onClick: () => clickOnPieChartSlice(ProcessField.DPIA, ProcessState.NO)
-                           },
-                           {
-                             label: `${intl.unclarified}`,
-                             color: colorUnknown,
-                             size: dashData.allProcesses.dpia.unknown,
-                             onClick: () => clickOnPieChartSlice(ProcessField.DPIA, ProcessState.UNKNOWN)
-                           },
-                         ]
-                       }/>
+                  data={
+                    [
+                      {
+                        label: `${intl.yes}`,
+                        color: colorYes,
+                        size: dashData.allProcesses.dpia.yes,
+                        onClick: () => clickOnPieChartSlice(ProcessField.DPIA, ProcessState.YES)
+                      },
+                      {
+                        label: `${intl.no}`,
+                        color: colorNo,
+                        size: dashData.allProcesses.dpia.no,
+                        onClick: () => clickOnPieChartSlice(ProcessField.DPIA, ProcessState.NO)
+                      },
+                      {
+                        label: `${intl.unclarified}`,
+                        color: colorUnknown,
+                        size: dashData.allProcesses.dpia.unknown,
+                        onClick: () => clickOnPieChartSlice(ProcessField.DPIA, ProcessState.UNKNOWN)
+                      },
+                    ]
+                  } />
               </Block>
 
               <Block marginTop={theme.sizing.scale600} marginRight={theme.sizing.scale600} minWidth={'520px'}>
                 <Chart title={intl.profiling} size={chartSize}
-                       data={
-                         [
-                           {
-                             label: `${intl.yes}`,
-                             color: colorYes,
-                             size: dashData.allProcesses.profiling?.yes || 0,
-                             onClick: () => clickOnPieChartSlice(ProcessField.PROFILING, ProcessState.YES)
-                           },
-                           {
-                             label: `${intl.no}`,
-                             color: colorNo,
-                             size: dashData.allProcesses.profiling?.no || 0,
-                             onClick: () => clickOnPieChartSlice(ProcessField.PROFILING, ProcessState.NO)
-                           },
-                           {
-                             label: `${intl.unclarified}`,
-                             color: colorUnknown,
-                             size: dashData.allProcesses.profiling?.unknown || 0,
-                             onClick: () => clickOnPieChartSlice(ProcessField.PROFILING, ProcessState.UNKNOWN)
-                           },
-                         ]
-                       }/>
+                  data={
+                    [
+                      {
+                        label: `${intl.yes}`,
+                        color: colorYes,
+                        size: dashData.allProcesses.profiling?.yes || 0,
+                        onClick: () => clickOnPieChartSlice(ProcessField.PROFILING, ProcessState.YES)
+                      },
+                      {
+                        label: `${intl.no}`,
+                        color: colorNo,
+                        size: dashData.allProcesses.profiling?.no || 0,
+                        onClick: () => clickOnPieChartSlice(ProcessField.PROFILING, ProcessState.NO)
+                      },
+                      {
+                        label: `${intl.unclarified}`,
+                        color: colorUnknown,
+                        size: dashData.allProcesses.profiling?.unknown || 0,
+                        onClick: () => clickOnPieChartSlice(ProcessField.PROFILING, ProcessState.UNKNOWN)
+                      },
+                    ]
+                  } />
               </Block>
 
               <Block marginTop={theme.sizing.scale600} marginRight={theme.sizing.scale600} minWidth={'520px'}>
                 <Chart title={intl.automaticProcessing} size={chartSize}
-                       data={
-                         [
-                           {
-                             label: `${intl.yes}`,
-                             color: colorYes,
-                             size: dashData.allProcesses.automation?.yes || 0,
-                             onClick: () => clickOnPieChartSlice(ProcessField.AUTOMATION, ProcessState.YES)
-                           },
-                           {
-                             label: `${intl.no}`,
-                             color: colorNo,
-                             size: dashData.allProcesses.automation?.no || 0,
-                             onClick: () => clickOnPieChartSlice(ProcessField.AUTOMATION, ProcessState.NO)
-                           },
-                           {
-                             label: `${intl.unclarified}`,
-                             color: colorUnknown,
-                             size: dashData.allProcesses.automation?.unknown || 0,
-                             onClick: () => clickOnPieChartSlice(ProcessField.AUTOMATION, ProcessState.UNKNOWN)
-                           },
-                         ]
-                       }/>
+                  data={
+                    [
+                      {
+                        label: `${intl.yes}`,
+                        color: colorYes,
+                        size: dashData.allProcesses.automation?.yes || 0,
+                        onClick: () => clickOnPieChartSlice(ProcessField.AUTOMATION, ProcessState.YES)
+                      },
+                      {
+                        label: `${intl.no}`,
+                        color: colorNo,
+                        size: dashData.allProcesses.automation?.no || 0,
+                        onClick: () => clickOnPieChartSlice(ProcessField.AUTOMATION, ProcessState.NO)
+                      },
+                      {
+                        label: `${intl.unclarified}`,
+                        color: colorUnknown,
+                        size: dashData.allProcesses.automation?.unknown || 0,
+                        onClick: () => clickOnPieChartSlice(ProcessField.AUTOMATION, ProcessState.UNKNOWN)
+                      },
+                    ]
+                  } />
               </Block>
 
               <Block marginTop={theme.sizing.scale600} minWidth={'520px'}>
                 <Chart title={intl.incompleteLegalBasis} size={chartSize}
-                       data={
-                         [
-                           {
-                             label: `${intl.numberOfProcessesWithUnknownLegalBasis}`,
-                             size: dashData.allProcesses.processesMissingLegalBases,
-                             onClick: () => clickOnPieChartSlice(ProcessField.MISSING_LEGAL_BASIS, ProcessState.YES)
-                           },
-                           {
-                             label: `${intl.numberOfProcessesWithoutArticle6LegalBasis}`,
-                             size: dashData.allProcesses.processesMissingArt6,
-                             onClick: () => clickOnPieChartSlice(ProcessField.MISSING_ARTICLE_6, ProcessState.YES)
-                           },
-                           {
-                             label: `${intl.numberOfProcessesWithoutArticle9LegalBasis}`,
-                             size: dashData.allProcesses.processesMissingArt9,
-                             onClick: () => clickOnPieChartSlice(ProcessField.MISSING_ARTICLE_9, ProcessState.YES)
-                           },
-                         ]
-                       }/>
+                  data={
+                    [
+                      {
+                        label: `${intl.numberOfProcessesWithUnknownLegalBasis}`,
+                        size: dashData.allProcesses.processesMissingLegalBases,
+                        onClick: () => clickOnPieChartSlice(ProcessField.MISSING_LEGAL_BASIS, ProcessState.YES)
+                      },
+                      {
+                        label: `${intl.numberOfProcessesWithoutArticle6LegalBasis}`,
+                        size: dashData.allProcesses.processesMissingArt6,
+                        onClick: () => clickOnPieChartSlice(ProcessField.MISSING_ARTICLE_6, ProcessState.YES)
+                      },
+                      {
+                        label: `${intl.numberOfProcessesWithoutArticle9LegalBasis}`,
+                        size: dashData.allProcesses.processesMissingArt9,
+                        onClick: () => clickOnPieChartSlice(ProcessField.MISSING_ARTICLE_9, ProcessState.YES)
+                      },
+                    ]
+                  } />
               </Block>
             </Block>
 
 
             <Block marginTop="2.5rem">
               <Card overrides={cardShadow}>
-                <ReactMarkdown source={settings?.frontpageMessage} escapeHtml={false}/>
+                <ReactMarkdown source={settings?.frontpageMessage} escapeHtml={false} />
               </Card>
             </Block>
 
             <Block width="100%" display="flex" alignContent="center" marginTop="2.5rem">
-              <LastEvents/>
+              <LastEvents />
             </Block>
           </>
         )
