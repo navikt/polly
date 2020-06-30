30c1d48a400cf0646476115c9469518abb7a3837
Andreas Skomedal <andreas.skomedal@nav.no>
2020-06-30 11:14:00 +0200

    Remove console log markdown

--- apps/frontend/src/components/common/Markdown.tsx
+++ apps/frontend/src/components/common/Markdown.tsx
@@ -6,7 +6,6 @@ import {endsWith} from 'lodash'
  * First paragraph in the markdown is rendered as a react fragment instead to avoid single word markdowns adding paragraph gutters
  */
 const renderParagraph = (props: any) => {
-  console.log(props)
   const ckey0 = props.children && props.children[0]?.key
   if (!endsWith(ckey0, '-1-1-0')) {
     return <p {...props}/>
