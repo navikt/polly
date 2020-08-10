554b07fae67c785ab5763efa5f8a79f23e10d5be
Andreas Skomedal <andreas.skomedal@nav.no>
2020-08-10 17:48:40 +0200

    Add state filter to dashboard - itest

--- apps/backend/polly-test/polly-test-integration/src/test/java/no/nav/data/polly/dashboard/DashboardIT.java
+++ apps/backend/polly-test/polly-test-integration/src/test/java/no/nav/data/polly/dashboard/DashboardIT.java
@@ -2,6 +2,7 @@ package no.nav.data.polly.dashboard;
 
 import no.nav.data.polly.IntegrationTestBase;
 import no.nav.data.polly.dashboard.dto.DashResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -16,14 +17,28 @@ class DashboardIT extends IntegrationTestBase {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Test
-    void getProcess() {
+    @BeforeEach
+    void setUp() {
         createAndSavePolicy(PURPOSE_CODE1, createAndSaveInformationType());
         var p2 = createAndSaveProcess(PURPOSE_CODE2);
         p2.getData().getDpia().setNeedForDpia(null);
         processRepository.save(p2);
+    }
 
+    @Test
+    void getProcess() {
         ResponseEntity<DashResponse> resp = restTemplate.getForEntity("/dash", DashResponse.class);
+        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DashResponse response = resp.getBody();
+        assertThat(response).isNotNull();
+        assertThat(get(response.getDepartmentProcesses(), d -> d.getDepartment().equals("DEP")).getProcessesInProgress()).isEqualTo(2L);
+        assertThat(response.getAllProcesses().getProcessesInProgress()).isEqualTo(2L);
+    }
+
+    @Test
+    void getProcessInPorgress() {
+        ResponseEntity<DashResponse> resp = restTemplate.getForEntity("/dash?filter=ALL", DashResponse.class);
         assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         DashResponse response = resp.getBody();
