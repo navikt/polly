60fa21d80cc8a2f5d7cd77ae1aff7865a50ba9c7
danishnav <danish.khan@nav.no>
2020-08-11 08:54:34 +0200

    Test commit

--- apps/frontend/src/components/InformationType/InformationtypeMetadata/index.tsx
+++ apps/frontend/src/components/InformationType/InformationtypeMetadata/index.tsx
@@ -1,27 +1,27 @@
 import * as React from 'react'
-import {useState} from 'react'
-import {Block} from 'baseui/block'
+import { useState } from 'react'
+import { Block } from 'baseui/block'
 
 import AccordionInformationType from './AccordionInformationType'
-import {Disclosure, Document, InformationType, Policy} from '../../../constants'
-import {intl, theme} from '../../../util'
+import { Disclosure, Document, InformationType, Policy } from '../../../constants'
+import { intl, theme } from '../../../util'
 import Metadata from './Metadata'
 import InformationtypePolicyTable from './InformationtypePolicyTable'
 import TableDisclosure from '../../common/TableDisclosure'
-import {DocumentTable} from './DocumentTable'
-import {Tab} from 'baseui/tabs'
-import {H4, ParagraphSmall} from 'baseui/typography'
-import {user} from '../../../service/User'
-import {InformationTypeBannerButtons} from '../InformationTypeBannerButtons'
+import { DocumentTable } from './DocumentTable'
+import { Tab } from 'baseui/tabs'
+import { H4, ParagraphSmall } from 'baseui/typography'
+import { user } from '../../../service/User'
+import { InformationTypeBannerButtons } from '../InformationTypeBannerButtons'
 import Button from '../../common/Button'
-import {CustomizedTabs} from '../../common/CustomizedTabs'
-import {tabOverride} from '../../common/Style'
-import {lastModifiedDate} from '../../../util/date-formatter'
-import {canViewAlerts} from '../../../pages/AlertEventPage'
-import {faExclamationCircle} from '@fortawesome/free-solid-svg-icons'
-import {useHistory} from 'react-router-dom'
-import {Spinner} from '../../common/Spinner'
-import {useQueryParam} from '../../../util/hooks'
+import { CustomizedTabs } from '../../common/CustomizedTabs'
+import { tabOverride } from '../../common/Style'
+import { lastModifiedDate } from '../../../util/date-formatter'
+import { canViewAlerts } from '../../../pages/AlertEventPage'
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
+import { useHistory } from 'react-router-dom'
+import { Spinner } from '../../common/Spinner'
+import { useQueryParam } from '../../../util/hooks'
 
 interface InformationtypeMetadataProps {
   informationtype: InformationType;
@@ -30,41 +30,41 @@ interface InformationtypeMetadataProps {
   documents?: Document[]
 }
 
-const Purposes = ({policies}: { policies: Policy[] }) => {
+const Purposes = ({ policies }: { policies: Policy[] }) => {
   const selectedPurpose = useQueryParam('purpose')
   const [accordion, setAccordion] = React.useState(!!selectedPurpose)
   return (
     <Block>
       <Block display="flex" justifyContent="flex-end">
         <Button onClick={() => setAccordion(!accordion)}
-                size="compact"
-                shape="pill"
-                kind='outline'
-                $style={{
-                  position: 'relative',
-                  marginTop: `-${theme.sizing.scale1200}`,
-                  marginLeft: `-${theme.sizing.scale400}`,
-                  right: theme.sizing.scale600,
-                  bottom: theme.sizing.scale600
-                }}
+          size="compact"
+          shape="pill"
+          kind='outline'
+          $style={{
+            position: 'relative',
+            marginTop: `-${theme.sizing.scale1200}`,
+            marginLeft: `-${theme.sizing.scale400}`,
+            right: theme.sizing.scale600,
+            bottom: theme.sizing.scale600
+          }}
         >
           {accordion ? intl.showAll : intl.groupByProcessingActivities}
         </Button>
       </Block>
       {accordion ?
-        <AccordionInformationType policies={policies}/>
-        : <InformationtypePolicyTable policies={policies} showPurpose={true}/>}
+        <AccordionInformationType policies={policies} />
+        : <InformationtypePolicyTable policies={policies} showPurpose={true} />}
     </Block>
   )
 }
 
-const Disclosures = ({disclosures}: { disclosures: Disclosure[] }) => {
+const Disclosures = ({ disclosures }: { disclosures: Disclosure[] }) => {
   return (
     <TableDisclosure
       list={disclosures}
       showRecipient
       editable={false}
-      onCloseModal={() => console.log('skal fjerrens også')}
+      onCloseModal={() => console.log('skal fjerrens også!')}
     />
   )
 }
@@ -79,16 +79,16 @@ export const InformationtypeMetadata = (props: InformationtypeMetadataProps) => 
           <Block display="flex" justifyContent="space-between">
             <H4 marginTop="0">{props.informationtype.name}</H4>
             {user.canWrite() && (
-              <InformationTypeBannerButtons id={props.informationtype.id}/>
+              <InformationTypeBannerButtons id={props.informationtype.id} />
             )}
           </Block>
 
-          <Metadata informationtype={props.informationtype}/>
+          <Metadata informationtype={props.informationtype} />
 
           <Block display='flex' justifyContent='flex-end' marginBottom={theme.sizing.scale600}>
             {canViewAlerts() && <Block marginRight='auto'>
               <Button type='button' kind='tertiary' size='compact' icon={faExclamationCircle}
-                      onClick={() => history.push(`/alert/events/informationtype/${props.informationtype.id}`)}>{intl.alerts}</Button>
+                onClick={() => history.push(`/alert/events/informationtype/${props.informationtype.id}`)}>{intl.alerts}</Button>
             </Block>}
             <ParagraphSmall>
               <i>{intl.formatString(intl.lastModified, props.informationtype.changeStamp.lastModifiedBy, lastModifiedDate(props.informationtype.changeStamp.lastModifiedDate))}</i>
@@ -100,16 +100,16 @@ export const InformationtypeMetadata = (props: InformationtypeMetadataProps) => 
             onChange={args => setActiveTab(args.activeKey as string)}
           >
             <Tab key="purposes" title={intl.processingActivityUse} overrides={tabOverride}>
-              {!props.policies && <Spinner size={theme.sizing.scale1200} margin={theme.sizing.scale1200}/>}
-              {props.policies && <Purposes policies={props.policies}/>}
+              {!props.policies && <Spinner size={theme.sizing.scale1200} margin={theme.sizing.scale1200} />}
+              {props.policies && <Purposes policies={props.policies} />}
             </Tab>
             <Tab key="disclose" title={intl.disclosuresToThirdParty} overrides={tabOverride}>
-              {!props.disclosures && <Spinner size={theme.sizing.scale1200} margin={theme.sizing.scale1200}/>}
-              {props.disclosures && <Disclosures disclosures={props.disclosures}/>}
+              {!props.disclosures && <Spinner size={theme.sizing.scale1200} margin={theme.sizing.scale1200} />}
+              {props.disclosures && <Disclosures disclosures={props.disclosures} />}
             </Tab>
             <Tab key="document" title={intl.documents} overrides={tabOverride}>
-              {!props.documents && <Spinner size={theme.sizing.scale1200} margin={theme.sizing.scale1200}/>}
-              {props.documents && <DocumentTable documents={props.documents}/>}
+              {!props.documents && <Spinner size={theme.sizing.scale1200} margin={theme.sizing.scale1200} />}
+              {props.documents && <DocumentTable documents={props.documents} />}
             </Tab>
           </CustomizedTabs>
         </>
