7376174e01839b73d91f42c1b74d61501215ab44
Shayan <shayan.yazdanmehr@nav.no>
2020-08-11 12:46:13 +0200

    adding processStatus as a parameter

--- apps/frontend/src/api/DashboardApi.ts
+++ apps/frontend/src/api/DashboardApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-import { env } from '../util/env'
-import { DashboardData } from '../constants'
+import {env} from '../util/env'
+import {DashboardData, ProcessStatus} from '../constants'
 
-export const getDashboard = async () => {
-  return (await axios.get<DashboardData>(`${env.pollyBaseUrl}/dash`)).data
+export const getDashboard = async (filter: ProcessStatus = ProcessStatus.All) => {
+  return (await axios.get<DashboardData>(`${env.pollyBaseUrl}/dash?filter=${filter}`)).data
 }
