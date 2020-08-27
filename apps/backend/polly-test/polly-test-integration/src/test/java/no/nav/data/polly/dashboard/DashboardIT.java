fdbe7300d17505c57a437c672b4eab3c07fac9f0
Andreas Skomedal <andreas.skomedal@nav.no>
2020-08-27 20:00:54 +0200

    Delete obsolete test

--- apps/backend/polly-test/polly-test-integration/src/test/java/no/nav/data/polly/dashboard/DashboardIT.java
+++ apps/backend/polly-test/polly-test-integration/src/test/java/no/nav/data/polly/dashboard/DashboardIT.java
@@ -37,17 +37,6 @@ class DashboardIT extends IntegrationTestBase {
     }
 
     @Test
-    void getDashNoSql() {
-        ResponseEntity<DashResponse> resp = restTemplate.getForEntity("/dash/nosql", DashResponse.class);
-        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        DashResponse response = resp.getBody();
-        assertThat(response).isNotNull();
-        assertThat(get(response.getDepartmentProcesses(), d -> d.getDepartment().equals("DEP")).getProcessesInProgress()).isEqualTo(2L);
-        assertThat(response.getAllProcesses().getProcessesInProgress()).isEqualTo(2L);
-    }
-
-    @Test
     void getDashForProcessInProgress() {
         ResponseEntity<DashResponse> resp = restTemplate.getForEntity("/dash?filter=IN_PROGRESS", DashResponse.class);
         assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
