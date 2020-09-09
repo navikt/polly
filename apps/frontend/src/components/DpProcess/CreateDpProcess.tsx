1d0f423b5564891b595076ef91cc2c6a43d2c100
Shayan <shayan.yazdanmehr@nav.no>
2020-09-09 13:26:36 +0200

    Adding sample pages as a starting point

---
+++ apps/frontend/src/components/DpProcess/CreateDpProcess.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {DpProcess} from "../../constants";
+
+let initialDpProcess:DpProcess
+
+const CreateDpProcess = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default CreateDpProcess
---
+++ apps/frontend/src/pages/DpProcessPage.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {Label1} from "baseui/typography";
+
+const DpProcessPage = () => {
+  return (
+    <>
+      <Label1>DpProcess!</Label1>
+    </>
+  )
+}
+
+export default DpProcessPage
