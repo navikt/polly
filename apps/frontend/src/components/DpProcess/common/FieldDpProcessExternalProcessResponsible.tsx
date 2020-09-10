94dd861b68c5fb22bb926bddfbf773dab4875727
Shayan <shayan.yazdanmehr@nav.no>
2020-09-10 11:20:26 +0200

    Adding fields for to the modal

---
+++ apps/frontend/src/components/DpProcess/common/FieldDpProcessExternalProcessResponsible.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import {Select, Value} from 'baseui/select'
+import {codelist, ListName} from '../../../service/Codelist'
+import {Field, FieldProps} from 'formik'
+import {DpProcessFormValues} from '../../../constants'
+import {Block} from 'baseui/block'
+
+const FieldDpProcessExternalProcessResponsible = (props: { thirdParty?: string, hideSelect: () => void }) => {
+  const {thirdParty} = props
+  const [value, setValue] = React.useState<Value>(thirdParty ? [{
+    id: thirdParty,
+    label: codelist.getShortname(ListName.THIRD_PARTY, thirdParty)
+  }] : [])
+
+  return (
+    <Field
+      name='commonExternalProcessResponsible'
+      render={({form}: FieldProps<DpProcessFormValues>) => (
+        <Block width={'100%'}>
+          <Select
+            options={codelist.getParsedOptions(ListName.THIRD_PARTY)}
+            onChange={({value}) => {
+              if (!value.length) props.hideSelect()
+              setValue(value)
+              form.setFieldValue('commonExternalProcessResponsible', value.length > 0 ? value[0].id : '')
+            }}
+            value={value}
+          />
+        </Block>
+      )}
+    />
+  )
+}
+
+export default FieldDpProcessExternalProcessResponsible
---
+++ apps/frontend/src/components/DpProcess/common/FieldDpProcessSubDepartment.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import {Select} from 'baseui/select'
+import {codelist, ListName} from '../../../service/Codelist'
+import {FieldArray, FormikProps} from 'formik'
+import {DpProcessFormValues} from '../../../constants'
+import {Block} from 'baseui/block'
+import {renderTagList} from "../../common/TagList"
+
+const FieldDpProcessSubDepartment = (props: { formikBag: FormikProps<DpProcessFormValues> }) => {
+
+  return (
+    <FieldArray
+      name='affiliation.subDepartments'
+      render={arrayHelpers => (
+        <Block width='100%'>
+          <Block width='100%'>
+            <Select
+              clearable
+              options={codelist.getParsedOptions(ListName.SUB_DEPARTMENT).filter(o => !props.formikBag.values.affiliation.subDepartments.includes(o.id))}
+              onChange={({value}) => {
+                arrayHelpers.form.setFieldValue('subDepartments', [...props.formikBag.values.affiliation.subDepartments, ...value.map(v => v.id)])
+              }}
+            />
+          </Block>
+          <Block>
+            <Block>{renderTagList(props.formikBag.values.affiliation.subDepartments.map(p => codelist.getShortname(ListName.SUB_DEPARTMENT, p)), arrayHelpers)}</Block>
+          </Block>
+        </Block>
+      )}
+    />
+  )
+
+}
+
+export default FieldDpProcessSubDepartment
