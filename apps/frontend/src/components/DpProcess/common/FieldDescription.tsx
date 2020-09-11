c382aa086d5b38db86ca72336deada73eb135573
Shayan <shayan.yazdanmehr@nav.no>
2020-09-11 15:02:46 +0200

    Adding description

---
+++ apps/frontend/src/components/DpProcess/common/FieldDescription.tsx
@@ -0,0 +1,17 @@
+import {Field, FieldProps} from "formik";
+import {DpProcessFormValues} from "../../../constants";
+import {Textarea} from "baseui/textarea";
+import {SIZE as InputSIZE} from "baseui/input";
+import * as React from "react";
+
+const FieldDescription = () => (
+  <Field
+    name='description'
+    render={({field, form}: FieldProps<string, DpProcessFormValues>) => (
+      <Textarea {...field} type='input' size={InputSIZE.default}
+                error={!!form.errors.description && form.touched.description}/>
+    )}
+  />
+)
+
+export default FieldDescription
