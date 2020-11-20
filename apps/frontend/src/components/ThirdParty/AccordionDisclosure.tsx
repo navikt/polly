b6acc0aef7b6d7260c28a402e48d525f0b398fb5
Shayan <shayan.yazdanmehr@nav.no>
2020-11-20 14:48:25 +0100

    Adding new panel to accordion component

--- apps/frontend/src/components/ThirdParty/AccordionDisclosure.tsx
+++ apps/frontend/src/components/ThirdParty/AccordionDisclosure.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react'
-import { useEffect, useState } from 'react'
-import { Disclosure, DisclosureAlert, DisclosureFormValues } from "../../constants";
-import { getAlertForDisclosure } from "../../api/AlertApi";
-import { mapDisclosureToFormValues } from "../../api";
-import ModalThirdParty from "./ModalThirdPartyForm";
-import { intl } from "../../util";
-import { Modal, ModalBody, ModalFooter, ModalHeader } from "baseui/modal";
-import { Paragraph2 } from "baseui/typography";
-import { Block } from "baseui/block";
-import Button from "../common/Button";
-import { useHistory } from 'react-router-dom'
-import { Accordion, Panel, StatelessAccordion } from "baseui/accordion";
+import {useEffect, useState} from 'react'
+import {Disclosure, DisclosureAlert, DisclosureFormValues} from "../../constants";
+import {getAlertForDisclosure} from "../../api/AlertApi";
+import {theme} from "../../util";
+import {Block} from "baseui/block";
+import {Accordion, Panel} from "baseui/accordion";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronRight} from "@fortawesome/free-solid-svg-icons";
 
 
 type AccordionDisclosureProps = {
@@ -53,8 +49,61 @@ const AccordionDisclosure = (props: AccordionDisclosureProps) => {
         {disclosureList && disclosureList.sort((a, b) => a.name.localeCompare(b.name)).map((d: Disclosure) => {
 
           return (
-            <Panel>
-              {d.name}
+            <Panel
+              title={
+                <Block>
+                  <FontAwesomeIcon icon={faChevronRight}/>
+                  <span> </span>
+                  <span>{d.name}</span>
+                </Block>
+              }
+              key={d.id}
+              overrides={{
+                ToggleIcon: {
+                  component: () => null
+                },
+                Content: {
+                  style: {
+                    backgroundColor: theme.colors.white,
+                    // Outline width
+                    paddingTop: '4px',
+                    paddingBottom: '4px',
+                    paddingLeft: '4px',
+                    paddingRight: '4px',
+                  }
+                }
+              }}
+            >
+              {/*{isLoading && <Block padding={theme.sizing.scale400}><StyledSpinnerNext size={theme.sizing.scale1200}/></Block>}*/}
+
+              {//!isLoading && currentProcess &&
+              (
+                <Block $style={{
+                  outline: `4px ${theme.colors.primary200} solid`
+                }}>
+
+                  <Block paddingLeft={theme.sizing.scale800} paddingRight={theme.sizing.scale800} paddingTop={theme.sizing.scale800}>
+                    {/*<ProcessData process={currentProcess}/>*/}
+
+                    <Block display='flex' justifyContent='flex-end'>
+                      {/*<span><i>{intl.formatString(intl.lastModified, currentProcess.changeStamp.lastModifiedBy, lastModifiedDate(currentProcess.changeStamp.lastModifiedDate))}</i></span>*/}
+                    </Block>
+                    <Block display='flex' paddingTop={theme.sizing.scale800} width='100%' justifyContent='flex-end'>
+                      {/*{canViewAlerts() && <Block marginRight='auto'>*/}
+                      {/*  <Button type='button' kind='tertiary' size='compact' icon={faExclamationCircle}*/}
+                      {/*          onClick={() => history.push(`/alert/events/process/${p.id}`)}>{intl.alerts}</Button>*/}
+                      {/*</Block>}*/}
+                      {/*{hasAccess() &&*/}
+                      {/*<Block>*/}
+                      {/*  <div ref={InformationTypeRef}></div>*/}
+                      {/*  {renderAddDocumentButton()}*/}
+                      {/*  {renderCreatePolicyButton()}*/}
+                      {/*</Block>*/}
+                      {/*}*/}
+                    </Block>
+                  </Block>
+                </Block>
+              )}
             </Panel>
           )
         })}
