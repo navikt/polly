42e666e8b00ee13faede98b7bda0db3ac3f406fc
Shayan <shayan.yazdanmehr@nav.no>
2020-11-23 11:16:25 +0100

    Adding delete functionality

--- apps/frontend/src/components/ThirdParty/AccordionDisclosure.tsx
+++ apps/frontend/src/components/ThirdParty/AccordionDisclosure.tsx
@@ -6,14 +6,18 @@ import {intl, theme} from "../../util";
 import {Block} from "baseui/block";
 import {Accordion, Panel} from "baseui/accordion";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faChevronRight, faExclamationCircle} from "@fortawesome/free-solid-svg-icons";
-import {getDisclosure} from "../../api";
+import {faChevronRight, faEdit, faExclamationCircle, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {getDisclosure, mapDisclosureToFormValues} from "../../api";
 import {StyledSpinnerNext} from "baseui/spinner";
 import DataText from "../common/DataText";
 import {DotTags} from "../common/DotTag";
 import {canViewAlerts} from "../../pages/AlertEventPage";
 import Button from "../common/Button";
 import {useHistory} from 'react-router-dom'
+import {KIND, SIZE} from "baseui/button";
+import ModalThirdParty from "./ModalThirdPartyForm";
+import {Modal, ModalBody, ModalFooter, ModalHeader} from "baseui/modal";
+import {Paragraph2} from "baseui/typography";
 
 
 type AccordionDisclosureProps = {
@@ -35,6 +39,7 @@ const AccordionDisclosure = (props: AccordionDisclosureProps) => {
   const [alerts, setAlerts] = useState<Alerts>({})
   const [isLoading, setLoading] = useState<boolean>(false)
   const history = useHistory()
+  const [hasAlert, setHasAlert] = useState<boolean>(false)
 
   const {disclosureList, showRecipient, submitDeleteDisclosure, submitEditDisclosure, errorModal, editable, onCloseModal} = props
 
@@ -48,6 +53,12 @@ const AccordionDisclosure = (props: AccordionDisclosureProps) => {
       setAlerts(alertMap)
     })()
   }, [disclosureList])
+
+  useEffect(() => {
+    if(selectedDisclosure){
+      setHasAlert(alerts[selectedDisclosure.id].missingArt6)
+    }
+  }, [selectedDisclosure])
 
   return (
     <React.Fragment>
@@ -97,16 +108,42 @@ const AccordionDisclosure = (props: AccordionDisclosureProps) => {
                 }}>
                   <Block padding={theme.sizing.scale800}>
                     <Block width='100%'>
+                      {editable &&
+                      <Block width="100%" display="flex" justifyContent="flex-end">
+                        <Button
+                          tooltip={intl.edit}
+                          size={SIZE.compact}
+                          kind={KIND.tertiary}
+                          onClick={() => {
+                            // setSelectedDisclosure(disclosure)
+                            setShowEditModal(true)
+                            console.log("edit")
+                          }}
+                          icon={faEdit}
+                        />
+
+                        <Button
+                          tooltip={intl.delete}
+                          size={SIZE.compact}
+                          kind={KIND.tertiary}
+                          onClick={() => {
+                            // setSelectedDisclosure(disclosure)
+                            setShowDeleteModal(true)
+                            console.log("delete")
+                          }}
+                          icon={faTrash}
+                        />
+                      </Block>}
                       <DataText label={intl.name} text={selectedDisclosure?.name}/>
                       <DataText label={intl.document} text={selectedDisclosure?.document?.name}/>
                       <DataText label={intl.description} text={selectedDisclosure?.description}/>
-                      <DataText label={intl.informationTypes} children={<DotTags items={selectedDisclosure?.informationTypes?.map(i=>i.name)}/>}/>
-                      <DataText label={intl.processes} children={<DotTags items={selectedDisclosure?.processes?.map(p=>p.name)}/>}/>
-                      {selectedDisclosure && alerts[selectedDisclosure.id].missingArt6 && canViewAlerts() &&
+                      <DataText label={intl.informationTypes} children={<DotTags items={selectedDisclosure?.informationTypes?.map(i => i.name)}/>}/>
+                      <DataText label={intl.processes} children={<DotTags items={selectedDisclosure?.processes?.map(p => p.name)}/>}/>
+                      {selectedDisclosure && hasAlert && canViewAlerts() &&
                       <DataText label={intl.alerts} children={
                         <Button type='button' kind='tertiary' size='compact'
-                                icon={faExclamationCircle} tooltip={alerts[selectedDisclosure.id].missingArt6 ? `${intl.alerts}: ${intl.MISSING_ARTICLE_6}` : `${intl.alerts}: ${intl.no}`}
-                                $style={{color: alerts[selectedDisclosure.id].missingArt6 ? theme.colors.warning500 : undefined}}
+                                icon={faExclamationCircle} tooltip={hasAlert ? `${intl.alerts}: ${intl.MISSING_ARTICLE_6}` : `${intl.alerts}: ${intl.no}`}
+                                $style={{color: hasAlert ? theme.colors.warning500 : undefined}}
                                 onClick={() => history.push(`/alert/events/disclosure/${selectedDisclosure.id}`)}
                         />
                       }/>
@@ -119,7 +156,52 @@ const AccordionDisclosure = (props: AccordionDisclosureProps) => {
           )
         })}
       </Accordion>
+      {editable && showEditModal && selectedDisclosure && (
+        <ModalThirdParty
+          title={intl.editDisclosure}
+          isOpen={showEditModal}
+          initialValues={mapDisclosureToFormValues(selectedDisclosure)}
+          submit={async (values) => submitEditDisclosure && await submitEditDisclosure(values) ? setShowEditModal(false) : setShowEditModal(true)}
+          onClose={() => {
+            onCloseModal && onCloseModal()
+            setShowEditModal(false)
+          }}
+          errorOnCreate={errorModal}
+          disableRecipientField={true}
+        />
+      )}
+      {showDeleteModal && (
+        <Modal
+          onClose={() => setShowDeleteModal(false)}
+          isOpen={showDeleteModal}
+          animate
+          unstable_ModalBackdropScroll={true}
+          size="default"
+        >
+          <ModalHeader>{intl.confirmDeleteHeader}</ModalHeader>
+          <ModalBody>
+            <Paragraph2>{intl.confirmDeletePolicyText} {selectedDisclosure?.name}</Paragraph2>
+          </ModalBody>
 
+          <ModalFooter>
+            <Block display="flex" justifyContent="flex-end">
+              <Block alignSelf="flex-end">{errorModal && <p>{errorModal}</p>}</Block>
+              <Button
+                kind="secondary"
+                onClick={() => setShowDeleteModal(false)}
+                marginLeft marginRight
+              >
+                {intl.abort}
+              </Button>
+              <Button onClick={() => {
+                if (selectedDisclosure)
+                  submitDeleteDisclosure && submitDeleteDisclosure(selectedDisclosure) ? setShowDeleteModal(false) : setShowDeleteModal(true)
+              }}
+              >{intl.delete}</Button>
+            </Block>
+          </ModalFooter>
+        </Modal>
+      )}
     </React.Fragment>
   )
 }
