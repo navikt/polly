b12e0c56e6c8d17ce75dc8337751f684e45492d0
Andreas Skomedal <andreas.skomedal@nav.no>
2020-12-09 15:44:09 +0100

    Fix interface

--- apps/frontend/src/constants.ts
+++ apps/frontend/src/constants.ts
@@ -302,7 +302,7 @@ export interface ProcessShort {
   id: string;
   name: string;
   number: number;
-  description: string;
+  description?: string;
   purposes: Code[];
   affiliation: Affiliation;
   status?: ProcessStatus;
