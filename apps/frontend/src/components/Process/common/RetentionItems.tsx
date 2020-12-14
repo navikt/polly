efbaa2c8b5363d21b990ebd9e814c60d856356c8
PaulMartinKokseter <59053738+PaulMartinKokseter@users.noreply.github.com>
2020-12-14 13:21:15 +0100

    Endret fra kassasjonsplan til kassasjonsvedtak. Flyttet en hjelpetekst.

--- apps/frontend/src/components/Process/common/RetentionItems.tsx
+++ apps/frontend/src/components/Process/common/RetentionItems.tsx
@@ -42,7 +42,7 @@ const RetentionItems = (props: { formikBag: FormikProps<ProcessFormValues> }) =>
   return (
     <>
       <Block {...rowBlockProps} marginTop={0}>
-        <ModalLabel label={intl.includeConservationPlan} tooltip={intl.retentionHelpText}/>
+        <ModalLabel label={intl.includeConservationPlan}/>
         <BoolField fieldName='retention.retentionPlan' value={formikBag.values.retention.retentionPlan}/>
       </Block>
 
@@ -75,7 +75,7 @@ const RetentionItems = (props: { formikBag: FormikProps<ProcessFormValues> }) =>
         <Error fieldName='retention.retentionMonths'/>
 
         <Block {...rowBlockProps}>
-          <ModalLabel label={intl.retentionStart}/>
+          <ModalLabel label={intl.retentionStart} tooltip={intl.retentionHelpText}/>
           <Block width={"100%"}>
             <FieldInput fieldName='retention.retentionStart'
                         fieldValue={formikBag.values.retention.retentionStart}/>
