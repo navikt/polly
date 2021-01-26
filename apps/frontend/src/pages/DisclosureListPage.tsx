648cef7acd7fb65bb611e6373ed6c0d89ff8ac63
Andreas Skomedal <andreas.skomedal@nav.no>
2021-01-26 11:09:38 +0100

    DisclosureListPage visibible filter

--- apps/frontend/src/pages/DisclosureListPage.tsx
+++ apps/frontend/src/pages/DisclosureListPage.tsx
@@ -1,28 +1,51 @@
 import React, {useEffect, useState} from 'react'
-import {H4} from 'baseui/typography'
-import {intl} from '../util'
+import {HeadingLarge, LabelMedium} from 'baseui/typography'
+import {intl, theme} from '../util'
 import {Disclosure} from '../constants'
 import {getAll, getDisclosuresByPageAndPageSize} from '../api'
 import AlphabeticList from '../components/common/AlphabeticList'
 import {useQueryParam} from '../util/hooks'
+import {Block} from 'baseui/block'
+import {Button as BButton} from 'baseui/button'
+import {ButtonGroup} from 'baseui/button-group'
+import {useHistory} from 'react-router-dom'
+import {lowerFirst} from 'lodash'
 
+enum FilterType {
+  legalbases = 'legalbases',
+  emptylegalbases = 'emptylegalbases'
+}
 
 export const DisclosureListPage = () => {
   const [disclosures, setDisclosures] = useState<Disclosure[]>([])
-  const filter = useQueryParam('filter')
+  const filter = useQueryParam<FilterType>('filter')
+  const history = useHistory()
 
   useEffect(() => {
     (async () => {
       const all = await getAll(getDisclosuresByPageAndPageSize)()
-      if (filter === 'emptylegalbases') setDisclosures(all.filter(d => !d.legalBases.length))
-      else if (filter === 'legalbases') setDisclosures(all.filter(d => !!d.legalBases.length))
+      if (filter === FilterType.emptylegalbases) setDisclosures(all.filter(d => !d.legalBases.length))
+      else if (filter === FilterType.legalbases) setDisclosures(all.filter(d => !!d.legalBases.length))
       else setDisclosures(all)
     })()
   }, [filter])
 
   return (
     <>
-      <H4>{intl.disclosures}</H4>
+      <Block display='flex' justifyContent='space-between' alignItems='center'>
+        <HeadingLarge>{intl.disclosures}</HeadingLarge>
+        <Block>
+          <LabelMedium marginBottom={theme.sizing.scale600}>{intl.filter} {lowerFirst(intl.legalBasisShort)}</LabelMedium>
+          <ButtonGroup
+            selected={!filter ? 0 : filter === FilterType.legalbases ? 1 : 2}
+            mode='radio' shape='pill'
+          >
+            <BButton onClick={() => history.replace("/disclosure")}>{intl.all}</BButton>
+            <BButton onClick={() => history.replace("/disclosure?filter=legalbases")}>{intl.filled}</BButton>
+            <BButton onClick={() => history.replace("/disclosure?filter=emptylegalbases")}>{intl.incomplete}</BButton>
+          </ButtonGroup>
+        </Block>
+      </Block>
 
       <AlphabeticList items={disclosures.map(dc => ({id: dc.id, label: dc.name}))} baseUrl={'/disclosure/'}/>
     </>
