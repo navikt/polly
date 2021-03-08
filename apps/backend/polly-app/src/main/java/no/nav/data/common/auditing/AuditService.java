9618c3b584ac3b1d5377279127bddf0a6c6761b7
Andreas Skomedal <andreas.skomedal@nav.no>
2021-03-08 19:44:10 +0100

    fix last edited query

--- apps/backend/polly-app/src/main/java/no/nav/data/common/auditing/AuditService.java
+++ apps/backend/polly-app/src/main/java/no/nav/data/common/auditing/AuditService.java
@@ -1,9 +1,7 @@
 package no.nav.data.common.auditing;
 
-import no.nav.data.common.auditing.domain.AuditVersion;
 import no.nav.data.common.auditing.domain.AuditVersionRepository;
 import no.nav.data.common.auditing.dto.AuditMetadata;
-import no.nav.data.polly.process.domain.Process;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +16,6 @@ public class AuditService {
     }
 
     public List<AuditMetadata> lastEditedProcessesByUser(String user) {
-        return auditVersionRepository.getLastChangesByUser(AuditVersion.tableName(Process.class), user, 20);
+        return auditVersionRepository.getLastChangedProcessesByUser(user, 20);
     }
 }
