5069c8d3bc8ca7ed40cd82f40eefc391bcfc642d
PaulMartinKokseter <59053738+PaulMartinKokseter@users.noreply.github.com>
2021-04-12 14:24:29 +0200

    Fjernet visning av tomme utleveringer fra BehandlingsView

--- apps/frontend/src/components/Process/Accordion/ProcessData.tsx
+++ apps/frontend/src/components/Process/Accordion/ProcessData.tsx
@@ -231,7 +231,8 @@ const ProcessData = (props: {process: Process, disclosures: Disclosure[]}) => {
         </Block>
       </DataText>
 
-      <DataText label={intl.disclosures} text={props.disclosures.length === 0 && intl.notFilled}>
+      {props.disclosures.length !== 0 &&
+      <DataText label={intl.disclosures} text={""}>
         <Block>
           {
             props.disclosures.map(value =>
@@ -243,6 +244,7 @@ const ProcessData = (props: {process: Process, disclosures: Disclosure[]}) => {
           }
         </Block>
       </DataText>
+      }
       <Completeness process={process}/>
 
       <DataText label={intl.status} text={""}>
