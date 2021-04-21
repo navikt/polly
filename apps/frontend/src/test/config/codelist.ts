8b5d7c7902f6ca0896aa4b26532cf01b4458d2a3
Andreas Skomedal <andreas.skomedal@nav.no>
2021-04-21 12:58:12 +0200

    more schema tests

--- apps/frontend/src/test/config/codelist.ts
+++ apps/frontend/src/test/config/codelist.ts
@@ -4,9 +4,13 @@ import {codelist, ListName} from '../../service/Codelist'
 codelist.lists = {
   codelist: {}
 }
-export const addCode = (list: ListName, code: string) => codelist.lists!.codelist[list] = [...(codelist.lists?.codelist[list] || []), {
-  list,
-  code,
-  shortName: code + ' name',
-  description: code + ' desc'
-}]
+export const addCode = (list: ListName, code: string) => {
+  const newCode = {
+    list,
+    code,
+    shortName: code + ' name',
+    description: code + ' desc'
+  }
+  codelist.lists!.codelist[list] = [...(codelist.lists?.codelist[list] || []), newCode]
+  return newCode
+}
