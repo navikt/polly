09dcfa6369788f3485afde1e6737a32fe6cb1a20
andre groseth <andregroseth@hotmail.com>
2021-04-29 10:42:40 +0200

    REFACTOR: Remove unused code
    Co-authored-by: JeremiahUy  <jeremiah.augie.uy@nav.no>

--- apps/backend/polly-app/src/main/java/no/nav/data/polly/codelist/codeusage/CodeUsageService.java
+++ apps/backend/polly-app/src/main/java/no/nav/data/polly/codelist/codeusage/CodeUsageService.java
@@ -196,7 +196,6 @@ public class CodeUsageService {
             case SUB_DEPARTMENT -> dpProcessRepository.findBySubDepartment(code);
             case SYSTEM -> dpProcessRepository.findByProduct(code);
             case THIRD_PARTY -> dpProcessRepository.findByExternalProcessResponsible(code);
-            case TRANSFER_GROUNDS_OUTSIDE_EU -> dpProcessRepository.findByTransferGroundsOutsideEU(code);
             default -> List.<DpProcess>of();
         }, DpProcess::convertToShortResponse);
     }
