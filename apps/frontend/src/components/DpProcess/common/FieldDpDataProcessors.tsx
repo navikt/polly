52ebbad6f640acc6b26a56ca61f0a5d59a38fcbb
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-04-29 13:56:06 +0200

    ADD: new field for dp process edit modal
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

---
+++ apps/frontend/src/components/DpProcess/common/FieldDpDataProcessors.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react'
+import {useEffect, useState} from 'react'
+import {FieldArray, FormikProps} from 'formik'
+import {Block} from 'baseui/block'
+import {DpProcessFormValues} from '../../../constants'
+import {Option, Select} from 'baseui/select'
+import {renderTagList} from '../../common/TagList'
+import {getProcessorsByIds} from '../../../api/ProcessorApi'
+import {intl} from '../../../util'
+
+type fieldDpDataProcessorsProps = {
+  formikBag: FormikProps<DpProcessFormValues>;
+  subDataProcessors?: Map<string, string>;
+  options: Option[]
+}
+
+const FieldDpDataProcessors = (props: fieldDpDataProcessorsProps) => {
+  const [subDataProcessors, setSubDataProcessors] = useState(props.subDataProcessors ? props.subDataProcessors : new Map<string, string>())
+
+  useEffect(() => {
+    (async () => {
+      if (props.formikBag.values.subDataProcessing.processors?.length) {
+        const res = await getProcessorsByIds(props.formikBag.values.subDataProcessing.processors)
+        res.forEach(r => subDataProcessors.set(r.id, r.name))
+      }
+    })()
+  }, [])
+
+  return <FieldArray
+    name='subDataProcessing.processors'
+    render={arrayHelpers => (
+      <>
+        <Block width='100%'>
+          <Block width='100%'>
+            <Select
+              clearable
+              noResultsMsg={intl.notFoundProcessor}
+              options={props.options.sort((a, b) => (a.label || '').toLocaleString().localeCompare((b.label || '').toLocaleString())).filter(dp => !props.formikBag.values.subDataProcessing.processors.includes(dp.id ? dp.id.toString() : ''))}
+              onChange={(params) => {
+                if (params.value[0].id && params.value[0].label) {
+                  subDataProcessors.set(params.value[0].id.toString(), params.value[0].label.toString())
+                }
+                arrayHelpers.form.setFieldValue('subDataProcessing.processors', [...props.formikBag.values.subDataProcessing.processors || [], ...params.value.map(v => v.id)])
+              }}
+            />
+          </Block>
+          <Block>{props.formikBag.values.subDataProcessing.processors && renderTagList(props.formikBag.values.subDataProcessing.processors.map(dp => {
+            let subDataProcessorName = ""
+            if (dp) {
+              if (subDataProcessors.has(dp)) {
+                subDataProcessorName = subDataProcessors.get(dp) || ""
+              }
+            }
+            return subDataProcessorName
+          }), arrayHelpers)}</Block>
+        </Block>
+      </>
+    )}
+  />
+}
+
+export default FieldDpDataProcessors
