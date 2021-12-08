1a8d15cb373ed06a1aa982b6ee014a1fb745bca5
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-12-08 16:27:29 +0100

    ADD: new code for SENSITIVITY codelist

---
+++ apps/backend/polly-app/src/main/resources/db/migration/V1.35__new_sensitivity_code.sql
@@ -0,0 +1,2 @@
+INSERT INTO CODELIST (LIST_NAME, CODE, SHORT_NAME, DESCRIPTION, CREATED_BY, CREATED_DATE, LAST_MODIFIED_BY, LAST_MODIFIED_DATE)
+values ('SENSITIVITY', 'IKKE_POL', 'Ikke personopplysning', 'Opplysningstype som ikke er personopplysning etter Personopplysningsloven, men som det er relevant å beskrive behandlingen av.', 'Datajegerne', now(), 'Datajegerne', now());
