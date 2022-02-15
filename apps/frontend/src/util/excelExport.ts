69dd82914bdefc801d522c11e7211ab73334259d
Shayan <shayan.yazdanmehr@nav.no>
2022-02-15 09:52:08 +0100

    bug fixing: Excel dependency

--- apps/frontend/src/util/excelExport.ts
+++ apps/frontend/src/util/excelExport.ts
@@ -1,29 +1,29 @@
-import XLSX from 'xlsx'
+import {utils, writeFile} from 'xlsx'
 import { intl } from '.'
 import { processStatusText } from '../components/Process/Accordion/ProcessData'
 import { ProcessShortWithEmail } from '../constants'
 
 export const handleExcelExport = (processes: ProcessShortWithEmail[], fileName: string) => {
-  const newWorkBook = XLSX.utils.book_new()
+  const newWorkBook = utils.book_new()
 
   const workSheetData: any[] = []
 
   processes.forEach((p) => {
     const newProcessObject: any= {}
-    
+
     newProcessObject[`${intl.process}`] = p.purposes[0].shortName + ': ' + p.name
     newProcessObject[`${intl.department}`] = p.affiliation.department?.shortName
     newProcessObject[`${intl.status}`] = processStatusText(p.status)
     newProcessObject[`${intl.commonExternalProcessResponsible}`] = p.commonExternalProcessResponsible?.shortName
     newProcessObject[`${intl.formatString(intl.lastModified, '', '').toString().slice(0, -2)}`] = p.changeStamp.lastModifiedBy
     newProcessObject[`${intl.email}`] = p.lastModifiedEmail
-    
+
     workSheetData.push(newProcessObject)
   })
 
   workSheetData.sort((a, b) => a.name > b.name ? 1 : -1)
 
-  XLSX.utils.book_append_sheet(newWorkBook, XLSX.utils.json_to_sheet(workSheetData))
-  XLSX.writeFile(newWorkBook, fileName + '.xlsx')
+  utils.book_append_sheet(newWorkBook, utils.json_to_sheet(workSheetData))
+  writeFile(newWorkBook, fileName + '.xlsx')
 }
 export default handleExcelExport
