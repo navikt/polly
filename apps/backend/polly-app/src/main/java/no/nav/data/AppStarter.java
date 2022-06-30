2aa99331550479c95544d1d83cf3db58ad7496c5
Shayan <shayan.yazdanmehr@nav.no>
2022-06-30 13:27:30 +0200

    Disabling logging default security password
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/backend/polly-app/src/main/java/no/nav/data/AppStarter.java
+++ apps/backend/polly-app/src/main/java/no/nav/data/AppStarter.java
@@ -2,10 +2,11 @@ package no.nav.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class AppStarter {
 
     public static void main(String[] args) {
--- apps/backend/polly-app/src/test/java/no/nav/data/polly/LocalAppStarter.java
+++ apps/backend/polly-app/src/test/java/no/nav/data/polly/LocalAppStarter.java
@@ -2,7 +2,10 @@ package no.nav.data.polly;
 
 
 import no.nav.data.AppStarter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class LocalAppStarter {
 
     public static void main(String[] args) {
