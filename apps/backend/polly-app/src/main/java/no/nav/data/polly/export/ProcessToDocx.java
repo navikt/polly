a8900231baf09c56fc2bb9f1869b0336de3ca629
Shayan <shayan.yazdanmehr@nav.no>
2022-09-06 14:55:18 +0200

    Test unicode
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>

--- apps/backend/polly-app/src/main/java/no/nav/data/polly/export/ProcessToDocx.java
+++ apps/backend/polly-app/src/main/java/no/nav/data/polly/export/ProcessToDocx.java
@@ -433,7 +433,7 @@ public class ProcessToDocx {
                                 Optional.ofNullable(pd.getTransferGroundsOutsideEUOther()).map(s -> ": " + s).orElse(""))
                         : text("");
 
-                addHeading4("•" + pd.getName());
+                addHeading4("\t• " + pd.getName());
                 if (documentAccess.equals(DocumentAccess.INTERNAL)) {
                     addTexts(
                             text("Ref. til databehandleravtale: ", nullToEmpty(pd.getContract())),
