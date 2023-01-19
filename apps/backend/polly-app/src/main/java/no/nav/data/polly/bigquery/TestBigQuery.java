52c580d4ddedc11cb611ac29096498ffdc77c79c
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-01-19 10:37:19 +0100

    ADD: method to parse data from bigquery to etterlevelseData
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>

--- apps/backend/polly-app/src/main/java/no/nav/data/polly/bigquery/TestBigQuery.java
+++ apps/backend/polly-app/src/main/java/no/nav/data/polly/bigquery/TestBigQuery.java
@@ -1,15 +1,23 @@
 package no.nav.data.polly.bigquery;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryException;
 import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.Field;
 import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.Schema;
 import com.google.cloud.bigquery.TableResult;
+import no.nav.data.polly.bigquery.domain.EtterlevelseData;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class TestBigQuery {
@@ -24,15 +32,17 @@ public class TestBigQuery {
     String query =
             "SELECT * "
                     + " FROM `"
-                    + projectId1
+                    + projectId
                     + "."
-                    + datasetName1
+                    + datasetName
                     + "."
-                    + tableName1
+                    + tableName
                     + "`"
                     + " LIMIT 10";
 
-    public void queryBigquery(String query) {
+    public List<EtterlevelseData> queryBigquery(String query) {
+        List<EtterlevelseData> etterlevelseDataList = new ArrayList<>();
+
         try {
             GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(getClass().getClassLoader().getResource("teamdatajegerne-prod-c8b1-ff4f59e3edd0.json").getPath()));
             BigQuery bigquery = BigQueryOptions.newBuilder().setCredentials(credentials).build().getService();
@@ -40,15 +50,25 @@ public class TestBigQuery {
             QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(query).build();
 
             TableResult results = bigquery.query(queryConfig);
-
+            Schema schema = results.getSchema();
             results
                     .iterateAll()
-                    .forEach(row -> row.forEach(val -> System.out.printf("%s,", val.toString())));
+                    .forEach(row -> {
+                        Map<String, String> m = new HashMap<>();
+                        for(Field field : schema.getFields()) {
+                            m.put(field.getName(), (String) row.get(field.getName()).getValue());
+                        }
+                        ObjectMapper mapper = new ObjectMapper();
+                        etterlevelseDataList.add(mapper.convertValue(m, EtterlevelseData.class));
+                    });
+
+            System.out.println(etterlevelseDataList);
 
             System.out.println("Query performed successfully.");
         } catch (BigQueryException | InterruptedException | IOException e) {
             System.out.println("Query not performed \n" + e.toString());
         }
+        return  etterlevelseDataList;
     }
 
     public String getDatasetName() {
