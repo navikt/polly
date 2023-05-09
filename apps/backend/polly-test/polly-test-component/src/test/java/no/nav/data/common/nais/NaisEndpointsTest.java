b655abe4c6aaa71bc6635142dac1c53732487af1
Shayan <shayan.yazdanmehr@nav.no>
2023-05-09 14:49:20 +0200

    Building Mvc manually! We might need to add .apply(springSecurity()) later

--- apps/backend/polly-test/polly-test-component/src/test/java/no/nav/data/common/nais/NaisEndpointsTest.java
+++ apps/backend/polly-test/polly-test-component/src/test/java/no/nav/data/common/nais/NaisEndpointsTest.java
@@ -3,6 +3,7 @@ package no.nav.data.common.nais;
 import io.micrometer.core.instrument.MeterRegistry;
 import no.nav.data.AppStarter;
 import no.nav.data.polly.codelist.CodelistRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -30,7 +33,14 @@ class NaisEndpointsTest {
     @MockBean
     private CodelistRepository codelistRepository;
     @Autowired
+    private WebApplicationContext applicationContext;
+
     private MockMvc mvc;
+
+    @BeforeEach
+    void init() {
+        mvc = MockMvcBuilders.webAppContextSetup(applicationContext).build();
+    }
 
     @Test
     void naisIsReady() throws Exception {
