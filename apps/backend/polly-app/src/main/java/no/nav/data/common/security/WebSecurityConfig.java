4799ddd7f5e0ed325fe9ec4ce51b6e62a6ae4f8e
Lord Andre Groseth <andregroseth@hotmail.com>
2023-05-25 12:26:01 +0200

    remove deprecated method
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    Co-authored-by: Shayan <shayan.yazdanmehr@nav.no>

--- apps/backend/polly-app/src/main/java/no/nav/data/common/security/WebSecurityConfig.java
+++ apps/backend/polly-app/src/main/java/no/nav/data/common/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -30,9 +31,9 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .logout().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         addFilters(http);
 
@@ -110,7 +111,7 @@ public class WebSecurityConfig {
     }
 
     private void addFilters(HttpSecurity http) {
-        // In lightweight mvc tests where authfilter isnt initialized
+        // In lightweight mvc tests where authfilter is not initialized
         if (aadAuthFilter != null) {
             http.addFilterBefore(aadAuthFilter, UsernamePasswordAuthenticationFilter.class);
         }
