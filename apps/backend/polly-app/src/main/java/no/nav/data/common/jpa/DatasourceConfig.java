7a7ea2c6b6003003a391c05a49581442bb03100f
Shayan <shayan.yazdanmehr@nav.no>
2023-06-12 13:15:36 +0200

    Migrating from onprem to GCP
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>

--- apps/backend/polly-app/src/main/java/no/nav/data/common/jpa/DatasourceConfig.java
+++ apps/backend/polly-app/src/main/java/no/nav/data/common/jpa/DatasourceConfig.java
@@ -2,13 +2,8 @@ package no.nav.data.common.jpa;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil;
-import no.nav.vault.jdbc.hikaricp.VaultError;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
@@ -16,37 +11,16 @@ import static java.util.concurrent.TimeUnit.MINUTES;
 @Slf4j
 @Configuration
 public class DatasourceConfig {
-
-    @Data
-    @Configuration
-    @ConfigurationProperties(prefix = "vault")
-    public static class VaultConfig {
-
-        private boolean enabled = true;
-        private String databaseBackend;
-        private String databaseRole;
-        private String databaseAdminrole;
-    }
-
-    @Bean
-    public HikariDataSource dataSource(DataSourceProperties properties, VaultConfig vaultConfig) throws VaultError {
-        HikariConfig config = createHikariConfig(properties);
-        if (vaultConfig.enabled) {
-            return HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(config, vaultConfig.databaseBackend, vaultConfig.databaseRole);
-        }
-        config.setUsername(properties.getUsername());
-        config.setPassword(properties.getPassword());
-        return new HikariDataSource(config);
-    }
-
-    static HikariConfig createHikariConfig(DataSourceProperties properties) {
+    static HikariDataSource hikariDataSource(DataSourceProperties properties) {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(properties.getUrl());
         config.setMinimumIdle(1);
         config.setMaximumPoolSize(2);
         config.setIdleTimeout(MINUTES.toMillis(5));
         config.setMaxLifetime(MINUTES.toMillis(9));
-        return config;
+        config.setUsername(properties.getUsername());
+        config.setPassword(properties.getPassword());
+        return new HikariDataSource(config);
     }
 
 }
--- apps/backend/polly-app/src/test/java/no/nav/data/polly/LocalAppStarter.java
+++ apps/backend/polly-app/src/test/java/no/nav/data/polly/LocalAppStarter.java
@@ -10,7 +10,6 @@ public class LocalAppStarter {
 
     public static void main(String[] args) {
         System.setProperty("spring.profiles.active", "local");
-        System.setProperty("ENVIRONMENT_CLASS", "preprod");
         AppStarter.main(args);
     }
 }
