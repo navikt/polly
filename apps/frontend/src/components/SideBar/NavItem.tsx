1106e7b66c7b0237e162e2eb239e83c3134ba63d
Shayan <shayan.yazdanmehr@nav.no>
2023-06-29 13:19:40 +0200

    Bug fixing: Chevron direction

--- apps/frontend/src/components/SideBar/NavItem.tsx
+++ apps/frontend/src/components/SideBar/NavItem.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import RouteLink from '../common/RouteLink'
-import { Block } from 'baseui/block'
-import { ParagraphMedium } from 'baseui/typography'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons'
-import { useLocation } from 'react-router-dom'
+import {Block} from 'baseui/block'
+import {ParagraphMedium} from 'baseui/typography'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faChevronDown, faChevronRight} from '@fortawesome/free-solid-svg-icons'
+import {useLocation} from 'react-router-dom'
 import CustomizedStatefulTooltip from '../common/CustomizedStatefulTooltip'
 
 interface NavItemProps {
@@ -13,11 +13,18 @@ interface NavItemProps {
   tooltip?: string
 }
 
+const checkCurrentLocationIsTheSameAsSideBarItem = (currentLocationUrl: string, sidebarItemUrl: string): boolean => {
+  if (sidebarItemUrl.slice(0, 2) === "//") {
+    return false
+  }
+  return currentLocationUrl.split('/')[1] === sidebarItemUrl.split('/')[1]
+}
+
 const NavItem = (props: NavItemProps) => (
-  <RouteLink href={props.to} style={{ textDecoration: 'none' }}>
+  <RouteLink href={props.to} style={{textDecoration: 'none'}}>
     <Block display="flex" alignItems="center" height={'35px'}>
       <Block marginRight="scale400">
-        <FontAwesomeIcon icon={useLocation().pathname.split('/')[1] === props.to.split('/')[1] ? faChevronDown : faChevronRight} color="white" size="lg" />
+        <FontAwesomeIcon icon={checkCurrentLocationIsTheSameAsSideBarItem(useLocation().pathname, props.to) ? faChevronDown : faChevronRight} color="white" size="lg"/>
       </Block>
       {!!props.tooltip ? (
         <CustomizedStatefulTooltip content={props.tooltip} ignoreBoundary={false}>
