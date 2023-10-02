10f1ef341ff56f4a523923ba63a2b4496f5bc69e
Shayan <shayan.yazdanmehr@nav.no>
2023-10-02 12:56:58 +0200

    Revert "Updating React-Markdown to version 9.0.0"
    This reverts commit c4ab0a22d511f66c07fbc6d89167535baa2c64ea.

--- apps/frontend/src/components/common/Markdown.tsx
+++ apps/frontend/src/components/common/Markdown.tsx
@@ -1,26 +1,18 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
-import {ParagraphMedium} from 'baseui/typography'
+import { PluggableList } from 'react-markdown/lib/react-markdown'
+import { ParagraphMedium } from 'baseui/typography'
 import remarkGfm from 'remark-gfm'
 import rehypeRaw from 'rehype-raw'
-import {StyledLink} from "baseui/link";
 
 /**
  * singleWord true remove paragraph wrapper for content
  */
-export const Markdown = ({singleWord, escapeHtml = true, verbatim, source}: { source?: string; escapeHtml?: boolean; singleWord?: boolean; verbatim?: boolean }) => {
+export const Markdown = ({ singleWord, escapeHtml = true, verbatim, source }: { source?: string; escapeHtml?: boolean; singleWord?: boolean; verbatim?: boolean }) => {
   const renderers = {
     p: (parProps: any) => (singleWord ? <React.Fragment {...parProps} /> : verbatim ? <p {...parProps} /> : <ParagraphMedium {...parProps} />),
-    a: (linkProps: any) => {
-      const {children, href} = linkProps
-      return <StyledLink href={href} target={"_blank"}>{children}</StyledLink>
-    },
-    href: (linkProps: any) => {
-      const {children, href} = linkProps
-      return <StyledLink href={href} target={"_blank"}>{children}</StyledLink>
-    }
   }
 
-  const htmlPlugins: any = escapeHtml ? [] : [rehypeRaw]
-  return <ReactMarkdown children={source || ''} components={renderers} remarkPlugins={[remarkGfm]} rehypePlugins={htmlPlugins}/>
+  const htmlPlugins: PluggableList | any = escapeHtml ? [] : [rehypeRaw]
+  return <ReactMarkdown children={source || ''} components={renderers} linkTarget="_blank" remarkPlugins={[remarkGfm]} rehypePlugins={htmlPlugins} />
 }
