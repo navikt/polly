8076b8ff00ef9de350adaf7a4fb8d7b83e5a30d5
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-10-26 11:19:14 +0200

    UPDATE: term frontpage link

--- apps/frontend/src/util/config.ts
+++ apps/frontend/src/util/config.ts
@@ -7,7 +7,7 @@ export const datajegerSlackLink = `slack://channel?team=${navSlackTeamId}&id=${b
 export const documentationLink = 'https://navikt.github.io/naka/behandlingskatalog'
 export const helpLink = 'https://navno.sharepoint.com/sites/intranett-personvern'
 
-export const termUrl = (termId: string) => `https://begrepskatalog.intern.nav.no/begrep/${termId}`
+export const termUrl = (termId: string) => `https://navno.sharepoint.com/sites/begreper/SitePages/Begrep.aspx?bid=${termId}`
 export const slackRedirectUrl = (c: string) => `https://slack.com/app_redirect?team=${navSlackTeamId}&channel=${c.toLowerCase()}`
 
 export const teamLink = (teamId: string) => `${env.teamKatBaseUrl}team/${teamId}`
