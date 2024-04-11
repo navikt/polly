058e271d0ab3e4467249a8c7c87197d5c68e8efd
Lord Andre Groseth <andregroseth@hotmail.com>
2024-04-11 11:12:50 +0200

    UPDATE: Modal to display different content based on admin/not admin
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>>

--- apps/frontend/src/components/Process/Accordion/DeleteProcessModal.tsx
+++ apps/frontend/src/components/Process/Accordion/DeleteProcessModal.tsx
@@ -1,8 +1,9 @@
 import { intl, theme } from '../../../util'
 import * as React from 'react'
 import { Disclosure, Process } from '../../../constants'
-import {BodyShort, Heading, List} from '@navikt/ds-react'
-import {Modal, Button} from '@navikt/ds-react'
+import { BodyShort, Heading, Link, List } from '@navikt/ds-react'
+import { Modal, Button } from '@navikt/ds-react'
+import { user } from '../../../service/User'
 
 interface DeleteProcessProps {
   onClose: () => void
@@ -17,35 +18,43 @@ export const DeleteProcessModal = (props: DeleteProcessProps) => {
   const { process, onClose, isOpen, submitDeleteProcess, errorProcessModal, disclosures } = props
 
   return (
-    <Modal header={{heading: intl.confirmDeleteHeader}} onClose={onClose} open={isOpen}>
+    <Modal header={{ heading: intl.confirmDeleteHeader }} onClose={onClose} open={isOpen}>
       <Modal.Body>
-        <BodyShort spacing>
-          {intl.deleteProcessText}
-        </BodyShort>
+        <BodyShort spacing>{intl.deleteProcessText}</BodyShort>
 
-        {!process.policies.length && (
-          <BodyShort spacing>
-            {intl.confirmDeleteProcessText} {process.name}
+        {!user.isAdmin() && (
+          <BodyShort>
+              For å slette behandlingen, kontakt oss på <Link target="_blank" href="https://nav-it.slack.com/archives/CR1B19E6L">slack</Link>.
           </BodyShort>
         )}
-        {(!!process.policies.length || !!disclosures.length) && (
-          <List as="ul" title={intl.deleteRelationText}>
-            {!!process.policies.length && <List.Item>{intl.formatString(intl.cannotDeleteProcess, process.name, '' + process.policies.length)}</List.Item>}
-            {!!disclosures.length && <List.Item>{intl.deleteProcessDisclosureError}</List.Item>}
-          </List>
+
+        {user.isAdmin() && (
+          <div>
+            {!process.policies.length && (
+              <BodyShort spacing>
+                {intl.confirmDeleteProcessText} {process.name}
+              </BodyShort>
+            )}
+            {(!!process.policies.length || !!disclosures.length) && (
+              <List as="ul" title={intl.deleteRelationText}>
+                {!!process.policies.length && <List.Item>{intl.formatString(intl.cannotDeleteProcess, process.name, '' + process.policies.length)}</List.Item>}
+                {!!disclosures.length && <List.Item>{intl.deleteProcessDisclosureError}</List.Item>}
+              </List>
+            )}
+          </div>
         )}
       </Modal.Body>
 
       <Modal.Footer>
-          <div className="self-end">{errorProcessModal && <BodyShort>{errorProcessModal}</BodyShort>}</div>
-          <Button
-            onClick={() => {
-              submitDeleteProcess(process).then(onClose)
-            }}
-            disabled={!!process.policies.length || !!disclosures.length}
-          >
-            {intl.delete}
-          </Button>
+        <div className="self-end">{errorProcessModal && <BodyShort>{errorProcessModal}</BodyShort>}</div>
+        <Button
+          onClick={() => {
+            submitDeleteProcess(process).then(onClose)
+          }}
+          disabled={(!!process.policies.length || !!disclosures.length) && user.isAdmin()}
+        >
+          {intl.delete}
+        </Button>
         <Button variant="secondary" onClick={onClose}>
           {intl.abort}
         </Button>
