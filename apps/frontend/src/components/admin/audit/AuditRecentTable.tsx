3215d3b2696a18596223356e03b9f36fc80c2a18
K153697 <paul.martin.kokseter@nav.no>
2024-07-24 13:43:40 +0200

    UPDATE:  Endre til designsystem

--- apps/frontend/src/components/admin/audit/AuditRecentTable.tsx
+++ apps/frontend/src/components/admin/audit/AuditRecentTable.tsx
@@ -1,26 +1,37 @@
-import { LabelLarge, LabelSmall } from 'baseui/typography'
 import React, { useEffect, useState } from 'react'
 import { getAudits } from '../../../api/AuditApi'
 import { AuditItem, ObjectType, PageResponse } from '../../../constants'
-import { theme } from '../../../util'
 import moment from 'moment'
-import { Pagination } from 'baseui/pagination'
-import { TriangleDown } from 'baseui/icon'
-import { Button, KIND } from 'baseui/button'
-import { PLACEMENT, StatefulPopover } from 'baseui/popover'
-import { StatefulMenu } from 'baseui/menu'
-import { Block } from 'baseui/block'
 import { AuditButton } from './AuditButton'
 import _ from 'lodash'
-import ReactJson from 'react-json-view'
-import { faBinoculars, faCode } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { AuditActionIcon } from './AuditComponents'
 import randomColor from 'randomcolor'
-import { StatefulSelect } from 'baseui/select'
 import { ObjectLink } from '../../common/RouteLink'
-import { Cell, HeadCell, Row, Table } from '../../common/Table'
-import CustomizedStatefulTooltip from '../../common/CustomizedStatefulTooltip'
+import {BodyShort, Button, Heading, Modal, Pagination, Select, Spacer, Table, Tooltip} from "@navikt/ds-react";
+import {JsonView} from "react-json-view-lite";
+
+const CodeView = ({ audit }: { audit: AuditItem }) => {
+  const [modalOpen, setModalOpen] = useState(false)
+
+  return (
+    <div>
+      <Button key={audit.id} onClick={() => setModalOpen(!modalOpen)} variant="tertiary">
+        Vis data
+      </Button>
+      <Modal
+        key={audit.id}
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        className="max-h-[75%] overflow-y-scroll"
+        header={{ heading: 'Data visning' }}
+      >
+        <Modal.Body>
+          <JsonView data={audit.data} />
+        </Modal.Body>
+      </Modal>
+    </div>
+  )
+}
 
 export const AuditRecentTable = (props: { show: boolean }) => {
   const [audits, setAudits] = useState<PageResponse<AuditItem>>({ content: [], numberOfElements: 0, pageNumber: 0, pages: 0, pageSize: 1, totalElements: 0 })
@@ -63,94 +74,111 @@ export const AuditRecentTable = (props: { show: boolean }) => {
     return null
   }
 
+  const tableOptions = Object.keys(ObjectType).map((ot) => ({ value: ot, label: ot }))
+
   return (
     <>
       <div className="flex justify-between mb-2">
-        <LabelLarge>Siste endringer</LabelLarge>
+        <Heading size="small">Siste endringer</Heading>
         <div className="w-72 flex justify-between">
-          <LabelSmall alignSelf="center" marginRight=".5rem">
-            Tabellnavn:{' '}
-          </LabelSmall>
-          <StatefulSelect size="compact" options={Object.keys(ObjectType).map((ot) => ({ id: ot, label: ot }))} onChange={(p) => setTable(p?.value[0]?.id as ObjectType)} />
+          <Select
+            label="Tabell:"
+            onChange={(e) => {
+              if (e.target.value === 'Codelist') {
+                setTable(e.target.value.toUpperCase() as ObjectType)
+              } else {
+                setTable(e.target.value as ObjectType)
+              }
+            }}
+          >
+            <option value="">Velg type for versjonering</option>
+            {tableOptions.map((tableOption, index) => (
+              <option key={index + '_' + tableOption.label} value={tableOption.value}>
+                {tableOption.label}
+              </option>
+            ))}
+          </Select>
         </div>
       </div>
 
-      <Table
-        emptyText='Ingen versjonering'
-        headers={
-          <>
-            <HeadCell $style={{ maxWidth: '13%' }} title='Tidspunkt' />
-            <HeadCell $style={{ maxWidth: '17%' }} title='Tabell' />
-            <HeadCell title='ID' />
-            <HeadCell title='Bruker' />
-          </>
-        }
-      >
-        {audits.content.map((audit, index) => {
-          const length = window.innerWidth > 1000 ? (window.innerWidth > 1200 ? 40 : 30) : 20
-          const rowNum = audits.pageNumber * audits.pageSize + index + 1
-          return (
-            <Row key={audit.id}>
-              <Cell $style={{ maxWidth: '13%' }}>
-                <div className="mr-2">{rowNum}</div>
-                <AuditButton kind="tertiary" id={audit.tableId} auditId={audit.id}>
-                  <CustomizedStatefulTooltip content={audit.time}>{moment(audit.time).fromNow()}</CustomizedStatefulTooltip>
-                </AuditButton>
-              </Cell>
-              <Cell $style={{ maxWidth: '17%' }}>
-                <AuditActionIcon action={audit.action} /> {audit.table}
-              </Cell>
-              <Cell>
-                <CustomizedStatefulTooltip content={audit.tableId}>
-                  <div className={`text-[${colors[audit.tableId]}]`} >{_.truncate(audit.tableId, { length })}</div>
-                </CustomizedStatefulTooltip>
-              </Cell>
-              <Cell $style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <div>{audit.user}</div>
-                <div>
-                  <ObjectLink id={audit.tableId} type={audit.table} audit={audit}>
-                    <Button size="compact" shape="round" kind="tertiary">
-                      <FontAwesomeIcon icon={faBinoculars} />
-                    </Button>
-                  </ObjectLink>
-                  <StatefulPopover overrides={{ Body: { style: { width: '80%' } } }} content={() => <ReactJson src={audit.data} name={null} />}>
-                    <Button size="compact" shape="round" kind="tertiary">
-                      <FontAwesomeIcon icon={faCode} />
-                    </Button>
-                  </StatefulPopover>
-                </div>
-              </Cell>
-            </Row>
-          )
-        })}
+      <Table size="large" zebraStripes>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader className="w-[13%]">Tidspunkt</Table.ColumnHeader>
+            <Table.ColumnHeader className="w-[17%]">Tabell</Table.ColumnHeader>
+            <Table.ColumnHeader>ID</Table.ColumnHeader>
+            <Table.ColumnHeader>Bruker</Table.ColumnHeader>
+            <Table.ColumnHeader aria-hidden></Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {audits.content.map((audit, index) => {
+            const length = window.innerWidth > 1000 ? (window.innerWidth > 1200 ? 40 : 30) : 20
+            const rowNum = audits.pageNumber * audits.pageSize + index + 1
+            return (
+              <Table.Row key={audit.id}>
+                <Table.HeaderCell className="w-[13%]" scope="row">
+                  <div className="flex">
+                  <div className="mr-2">{rowNum}</div>
+                    <AuditButton kind="tertiary" id={audit.tableId} auditId={audit.id}>
+                      <Tooltip content={audit.time} placement="top">
+                        <div>{moment(audit.time).fromNow()}</div>
+                      </Tooltip>
+                    </AuditButton>
+                  </div>
+                </Table.HeaderCell>
+                <Table.HeaderCell className="w-[17%]">
+                  <AuditActionIcon action={audit.action}/> {audit.table}
+                </Table.HeaderCell>
+                <Table.DataCell>
+                  <Tooltip content={audit.tableId} placement="top">
+                    <div className={`text-[${colors[audit.tableId]}]`}>{_.truncate(audit.tableId, {length})}</div>
+                  </Tooltip>
+                </Table.DataCell>
+                <Table.DataCell>
+                  <div>{audit.user}</div>
+                </Table.DataCell>
+                <Table.HeaderCell>
+                  <div className="flex">
+                    <ObjectLink id={audit.tableId} type={audit.table} audit={audit}>
+                      <Button variant="tertiary">Finn bruk</Button>
+                    </ObjectLink>
+                    <CodeView audit={audit}/>
+                  </div>
+                </Table.HeaderCell>
+              </Table.Row>
+            )
+          })}
+        </Table.Body>
       </Table>
 
-      <div className="flex justify-between mt-4">
-        <StatefulPopover
-          content={({ close }) => (
-            <StatefulMenu
-              items={[5, 10, 20, 50, 100].map((i) => ({ label: i }))}
-              onItemSelect={({ item }) => {
-                setLimit(item.label)
-                close()
-              }}
-              overrides={{
-                List: {
-                  style: { height: '150px', width: '100px' },
-                },
-              }}
-            />
-          )}
-          placement={PLACEMENT.bottom}
+      <div className="flex w-full justify-center items-center mt-3">
+        <Select
+          label="Antall rader:"
+          value={limit}
+          onChange={(e) => {
+            setLimit(parseInt(e.target.value))
+          }}
+          size="small"
         >
-          <Button kind={KIND.tertiary} endEnhancer={TriangleDown}>{`${limit} Rader`}</Button>
-        </StatefulPopover>
-        <Pagination
-          currentPage={page}
-          numPages={audits.pages}
-          onPageChange={({ nextPage }) => handlePageChange(nextPage)}
-          labels={{ nextButton: 'Neste', preposition: 'av', prevButton: 'Fordige' }}
-        />
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </Select>
+        <Spacer/>
+        <div>
+          <Pagination
+            page={page}
+            onPageChange={(page) => handlePageChange(page)}
+            count={audits.pages}
+            prevNextTexts
+            size="small"
+          />
+        </div>
+        <Spacer/>
+        <BodyShort>Totalt antall rader: {audits.totalElements}</BodyShort>
       </div>
     </>
   )
