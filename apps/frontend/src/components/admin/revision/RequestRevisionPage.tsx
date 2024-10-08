df3f4c071856b00ab9ccbcf0daeefcc4050d9ee8
K153697 <paul.martin.kokseter@nav.no>
2024-10-08 12:37:22 +0200

    Endret til Tabs

--- apps/frontend/src/components/admin/revision/RequestRevisionPage.tsx
+++ apps/frontend/src/components/admin/revision/RequestRevisionPage.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {Field, Form, Formik} from 'formik'
+import { Form, Formik} from 'formik'
 import {useState} from 'react'
 import * as yup from 'yup'
 import {searchProcessOptions, useAllAreas} from '../../../api'
@@ -10,7 +10,7 @@ import { env } from '../../../util/env'
 import { Error } from '../../common/ModalSchema'
 import AsyncSelect from 'react-select/async'
 
-import {Alert, Button, Heading, Label, Loader, Radio, RadioGroup, Select, Textarea} from '@navikt/ds-react'
+import {Alert, Button, Heading, Label, Loader, Radio, RadioGroup, Select, Tabs, Textarea} from '@navikt/ds-react'
 
 const initialValues: ProcessRevisionRequest = {
   processSelection: RecipientType.ONE,
@@ -69,6 +69,20 @@ export const RequestRevisionPage = (props: IRequestRevisionPageProps) => {
       setError(undefined)
   }
 
+  const selectOnlyCompleted = (formikBag: any)=> {
+    return (
+      <RadioGroup
+        value={formikBag.values.completedOnly}
+        className="flex w-full mt-4"
+        legend="Velg kun fullførte behandlinger"
+        onChange={(value) => formikBag.setFieldValue('completedOnly', value)}
+        error={formikBag.errors.completedOnly}
+      >
+        <Radio value={true}>Velg kun fullførte behandlinger</Radio>
+        <Radio value={false}>Velg alle behandlinger</Radio>
+      </RadioGroup>)
+  }
+
   return (
     <div>
       <Heading level="1" size="large">Send anmodning om revidering</Heading>
@@ -93,108 +107,94 @@ export const RequestRevisionPage = (props: IRequestRevisionPageProps) => {
                   <Alert variant="success">Revidering etterspurt</Alert>
                 </div>
               }
-
                 <div className="flex w-full mt-4">
-                  <Field name="processSelection">
-                    {() => {
-                      return (
-                          <div className="min-w-fit">
-                            <RadioGroup
-                              value={formikBag.values.processSelection}
-                              legend="Velg omfang"
-                              onChange={(val) => formikBag.setFieldValue('processSelection', val)}
-                              name='processSelection'
-                              error={formikBag.errors.processSelection}
-                            >
-                              <Radio value={RecipientType.ONE}>Én behandling</Radio>
-                              <Radio value={RecipientType.ALL}>Alle</Radio>
-                              <Radio value={RecipientType.DEPARTMENT}>Avdeling</Radio>
-                              <Radio value={RecipientType.PRODUCT_AREA}>Område</Radio>
-                            </RadioGroup>
-                          </div>
-                      )
-                    }}
-                  </Field>
                 </div>
 
-              {formikBag.values.processSelection !== RecipientType.ONE && (
-                <RadioGroup
-                  value={formikBag.values.completedOnly}
-                  className="flex w-full mt-4"
-                  legend="Velg kun fullførte behandlinger"
-                  onChange={(value) => formikBag.setFieldValue('completedOnly', value)}
-                  error={formikBag.errors.completedOnly}
-                >
-                  <Radio value={true}>Velg kun fullførte behandlinger</Radio>
-                  <Radio value={false}>Velg alle behandlinger</Radio>
-                </RadioGroup>
-              )}
+              <Tabs defaultValue="logg" value={formikBag.values.processSelection} onChange={(value)=>formikBag.setFieldValue('processSelection', value)}>
+                <Tabs.List>
+                  <Tabs.Tab
+                    value={RecipientType.ONE}
+                    label="Én behandling"
+                  />
+                  <Tabs.Tab
+                    value={RecipientType.ALL}
+                    label="Alle behandlinger"
+                  />
+                  <Tabs.Tab
+                    value={RecipientType.DEPARTMENT}
+                    label="Avdeling"
+                  />
+                  <Tabs.Tab
+                    value={RecipientType.PRODUCT_AREA}
+                    label="Område"
+                  />
+                </Tabs.List>
+                <Tabs.Panel value={RecipientType.ONE} className="h-48 w-full p-4">
+                  <div className="flex w-full mt-4 my-3">
+                    <div className="my-3">
+                      <Label>Legg til en behandling fra Behandlingskatalogen</Label>
 
-              {formikBag.values.processSelection === RecipientType.ONE && (
-                <div className="flex w-full mt-4 my-3">
-                        <div className="my-3">
-                          <Label>Legg til  en behandling fra Behandlingskatalogen</Label>
+                      <AsyncSelect
+                        aria-label="Søk etter behandlinger"
+                        placeholder=""
+                        noOptionsMessage={() => 'Skriv minst 3 tegn for å søke'}
+                        loadingMessage={() => 'Søker...'}
+                        isClearable={true}
+                        loadOptions={searchProcessOptions}
+                        onChange={(val) => {
+                          if (val) {
+                            formikBag.setFieldValue('processId', val.value)
+                          }
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <Error fieldName="processId" fullWidth />
+                </Tabs.Panel>
+                <Tabs.Panel value={RecipientType.ALL} className="h-48 w-full p-4">
+                  {selectOnlyCompleted(formikBag)}
+                </Tabs.Panel>
+                <Tabs.Panel value={RecipientType.DEPARTMENT} className="h-68 w-full p-4">
+                  <Select
+                    className="w-1/2"
+                    label="Avdeling"
+                    onChange={(ev) => formikBag.setFieldValue('department', ev.currentTarget.value)}
+                    value={formikBag.values.department!}
+                    error={formikBag.errors.department}
+                  >
+                    <option key="" value="">
+                      Velg avdeling
+                    </option>
+                    {departments.map((codeListOption) => (
+                      <option key={`option_${codeListOption.id}`} value={codeListOption.label}>
+                        {codeListOption.label}
+                        </option>
+                      ))}
+                    </Select>
+                  {selectOnlyCompleted(formikBag)}
+                </Tabs.Panel>
+                <Tabs.Panel value={RecipientType.PRODUCT_AREA} className="h-68 w-full p-4">
+                  <Select
+                    className="w-1/2"
+                    label="Område"
+                    onChange={(ev) => formikBag.setFieldValue('productAreaId', ev.currentTarget.value)}
+                    value={formikBag.values.productAreaId!}
+                    error={formikBag.errors.productAreaId}
+                  >
+                    <option key="" value="">
+                      Velg område
+                    </option>
+                    {areas.map((productArea) => (
+                      <option key={`option_${productArea.id}`} value={productArea.name}>
+                        {productArea.name}
+                      </option>
+                    ))}
+                  </Select>
+                  {selectOnlyCompleted(formikBag)}
+                </Tabs.Panel>
+              </Tabs>
 
-                            <AsyncSelect
-                              aria-label="Søk etter behandlinger"
-                              placeholder=""
-                              noOptionsMessage={()=>'Skriv minst 3 tegn for å søke'}
-                              loadingMessage={() => 'Søker...'}
-                              isClearable={true}
-                              loadOptions={searchProcessOptions}
-                              onChange={(val) => {
-                                if (val) {
-                                  formikBag.setFieldValue('processId', val.value)
-                                }
-                              }}
-                            />
-                          </div>
-                </div>
-              )}
-              {error}
-              {formikBag.errors.processSelection}
-              <Error fieldName="processId" fullWidth />
-
-
-              {formikBag.values.processSelection === RecipientType.DEPARTMENT && (
-              <Select
-                className="w-1/2"
-                label="Avdeling"
-                onChange={(ev) => formikBag.setFieldValue('department', ev.currentTarget.value)}
-                value={formikBag.values.department!}
-                error={formikBag.errors.department}
-              >
-                <option key="" value="">
-                  Velg avdeling
-                </option>
-                {departments.map((codeListOption) => (
-                  <option key={`option_${codeListOption.id}`} value={codeListOption.label}>
-                    {codeListOption.label}
-                  </option>
-                ))}
-              </Select>
-              )}
-
-              {formikBag.values.processSelection === RecipientType.PRODUCT_AREA && (
-              <Select
-                className="w-1/2"
-                label="Område"
-                onChange={(ev) => formikBag.setFieldValue('productAreaId', ev.currentTarget.value)}
-                value={formikBag.values.productAreaId!}
-                error={formikBag.errors.productAreaId}
-              >
-                <option key="" value="">
-                  Velg område
-                </option>
-                {areas.map((productArea) => (
-                  <option key={`option_${productArea.id}`} value={productArea.name}>
-                    {productArea.name}
-                  </option>
-                ))}
-              </Select>
-                )}
-
-              <div className="flex mt-4">
+              <div className="flex mt-4 ml-4">
                   <Textarea
                     className="w-1/2"
                     name="revisionText"
@@ -204,7 +204,6 @@ export const RequestRevisionPage = (props: IRequestRevisionPageProps) => {
                     error={formikBag.errors.revisionText}
                     minRows={6}
                   />
-
               </div>
 
                 <div className="flex justify-end mt-6 gap-2">
