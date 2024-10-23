1ed4e5206c1389d07abf1dd9b69804c20bf46a67
beeccy <beeccy@users.noreply.github.com>
2024-10-23 11:58:43 +0200

    Byttet ut codelist med codelistUtils FieldDpProcessExternalProcessResponsible

--- apps/frontend/src/components/DpProcess/common/FieldDpProcessExternalProcessResponsible.tsx
+++ apps/frontend/src/components/DpProcess/common/FieldDpProcessExternalProcessResponsible.tsx
@@ -2,7 +2,7 @@ import { Select, Value } from 'baseui/select'
 import { Field, FieldProps } from 'formik'
 import { useState } from 'react'
 import { IDpProcessFormValues } from '../../../constants'
-import { EListName, codelist } from '../../../service/Codelist'
+import { CodelistService, EListName } from '../../../service/Codelist'
 
 interface IFieldDpProcessExternalProcessResponsible {
   thirdParty?: string
@@ -12,12 +12,14 @@ const FieldDpProcessExternalProcessResponsible = (
   props: IFieldDpProcessExternalProcessResponsible
 ) => {
   const { thirdParty } = props
+  const [codelistUtils] = CodelistService()
+
   const [value, setValue] = useState<Value>(
     thirdParty
       ? [
           {
             id: thirdParty,
-            label: codelist.getShortname(EListName.THIRD_PARTY, thirdParty),
+            label: codelistUtils.getShortname(EListName.THIRD_PARTY, thirdParty),
           },
         ]
       : []
@@ -29,7 +31,7 @@ const FieldDpProcessExternalProcessResponsible = (
       render={({ form }: FieldProps<IDpProcessFormValues>) => (
         <div className="w-full">
           <Select
-            options={codelist.getParsedOptions(EListName.THIRD_PARTY)}
+            options={codelistUtils.getParsedOptions(EListName.THIRD_PARTY)}
             onChange={({ value }) => {
               setValue(value)
               form.setFieldValue('externalProcessResponsible', value.length > 0 ? value[0].id : '')
