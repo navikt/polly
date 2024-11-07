b75b94859217451f779be83d10165efd1412b0f5
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-11-07 13:54:06 +0100

    REMOVE: uneeded codelist service

--- apps/frontend/src/test/config/codelist.ts
+++ apps/frontend/src/test/config/codelist.ts
@@ -1,18 +1,12 @@
 // Mock codelist
-import { CodelistService, EListName } from '../../service/Codelist'
+import { EListName } from '../../service/Codelist'
 
 export const addCode = (list: EListName, code: string) => {
-  const [, lists, setLists] = CodelistService()
-
-  setLists({ codelist: {} })
   const newCode = {
     list,
     code,
     shortName: code + ' name',
     description: code + ' desc',
-  }
-  if (lists) {
-    lists.codelist[list] = [...(lists.codelist[list] || []), newCode]
   }
   return newCode
 }
