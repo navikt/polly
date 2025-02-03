f98c11f96f2b6375b2b07133eff9663fa6693d5d
beeccy <beeccy@users.noreply.github.com>
2025-02-03 10:22:08 +0100

    Lagt ved alle endringer fra etterlevelses tailwind oppgradering

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,12 +1,14 @@
+import dsTailwind from '@navikt/ds-tailwind'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", '!./node_modules'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-  presets: [require("@navikt/ds-tailwind")]
+  presets: [dsTailwind]
 }
 
