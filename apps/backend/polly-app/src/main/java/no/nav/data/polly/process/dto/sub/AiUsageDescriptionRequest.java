085931e0c992bc56640a4a02daf99954d72ebf6a
JeremiahUy <jeremiah.uy@outlook.com>
2025-07-23 11:56:53 +0200

    UPDATE: ai usage fields

--- apps/backend/polly-app/src/main/java/no/nav/data/polly/process/dto/sub/AiUsageDescriptionRequest.java
+++ apps/backend/polly-app/src/main/java/no/nav/data/polly/process/dto/sub/AiUsageDescriptionRequest.java
@@ -1,21 +1,50 @@
-package no.nav.data.polly.process.domain.sub;
+package no.nav.data.polly.process.dto.sub;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
+import no.nav.data.common.utils.DateUtil;
+import no.nav.data.common.validator.FieldValidator;
+import no.nav.data.common.validator.Validated;
+import no.nav.data.polly.process.domain.sub.AiUsageDescription;
 
-import java.time.LocalDate;
+import static org.apache.commons.lang3.StringUtils.trimToNull;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AiUsageDescription {
+@FieldNameConstants
+public class AiUsageDescriptionRequest implements Validated {
     private Boolean aiUsage;
     private String description;
     private Boolean reusingPersonalInformation;
-    private LocalDate startDate;
-    private LocalDate endDate;
+    private String startDate;
+    private String endDate;
     private String registryNumber;
+
+    @Override
+    public void format() {
+        setDescription(trimToNull(description));
+        setRegistryNumber(trimToNull(registryNumber));
+    }
+
+    @Override
+    public void validate(FieldValidator validator) {
+        validator.checkDate(Fields.startDate, startDate);
+        validator.checkDate(Fields.endDate, endDate);
+    }
+
+    public AiUsageDescription convertToAiUsageDescription() {
+        return AiUsageDescription.builder()
+                .aiUsage(aiUsage)
+                .description(description)
+                .reusingPersonalInformation(reusingPersonalInformation)
+                .startDate(DateUtil.parseStart(startDate))
+                .endDate(DateUtil.parseEnd(endDate))
+                .registryNumber(registryNumber)
+                .build();
+    }
 }
