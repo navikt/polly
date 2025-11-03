fd91744b24bfef9ccb026db3f42cc3234f22c6e3
K153697 <paul.martin.kokseter@nav.no>
2025-11-03 12:28:18 +0100

    Fjernet Amplitude.ts og alle referanser

--- apps/frontend/src/util/env.ts
+++ apps/frontend/src/util/env.ts
@@ -4,8 +4,6 @@ export const env = {
   lovdataForskriftBaseUrl: process.env.REACT_APP_LOVDATA_FORSKRIFT_BASE_URL,
   teamKatBaseUrl: process.env.REACT_APP_TEAMKAT_BASE_URL,
   slackId: process.env.REACT_APP_SLACK_ID,
-  amplitudeEndpoint: process.env.REACT_APP_AMPLITUDE_ENDPOINT,
-  amplitudeApiKey: process.env.REACT_APP_AMPLITUDE_API_KEY,
   defaultStartDate: process.env.REACT_APP_DEFAULT_START_DATE || '0001-01-01',
   disableRiskOwner: process.env.REACT_APP_DISABLE_RISK_OWNER,
   disableDpProcess: process.env.REACT_APP_DISABLE_DP_PROCESS,
