7785b8bd1e22e4338b4bbedf780afed0d1ff0c3b
JeremiahUy <jeremiah.uy@outlook.com>
2025-11-25 14:44:36 +0100

    FIX: typo
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/backend/polly-app/src/main/java/no/nav/data/polly/process/domain/sub/nomSeksjon.java
+++ apps/backend/polly-app/src/main/java/no/nav/data/polly/process/domain/sub/nomSeksjon.java
@@ -11,6 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class nomSeksjon {
 
-    private String nomSeksjontId;
+    private String nomSeksjonId;
     private String nomSeksjonName;
 }
