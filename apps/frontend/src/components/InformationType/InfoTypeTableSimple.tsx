f5f849ec5d1c8f58ecd43fb43d5c6c88d99ec186
K153697 <paul.martin.kokseter@nav.no>
2026-01-09 12:55:13 +0100

    Erstattet Baseui Typografi og Table  med Aksel

--- apps/frontend/src/components/InformationType/InfoTypeTableSimple.tsx
+++ apps/frontend/src/components/InformationType/InfoTypeTableSimple.tsx
@@ -1,13 +1,10 @@
-import { HeadingXLarge } from 'baseui/typography'
+import { Heading, Loader, SortState, Table } from '@navikt/ds-react'
 import { useEffect, useState } from 'react'
-import { IInformationType, informationTypeSort } from '../../constants'
+import { IInformationType } from '../../constants'
 import { CodelistService, EListName } from '../../service/Codelist'
-import { theme } from '../../util'
-import { useTable } from '../../util/hooks'
+import { handleSort } from '../../util/handleTableSort'
 import { DotTags } from '../common/DotTag'
 import RouteLink from '../common/RouteLink'
-import { Spinner } from '../common/Spinner'
-import { Cell, HeadCell, Row, Table } from '../common/Table'
 import { Sensitivity } from './Sensitivity'
 
 type TTableProps = {
@@ -18,23 +15,40 @@ type TTableProps = {
 
 export const InfoTypeTable = ({ informationTypes, getInfoTypes, title }: TTableProps) => {
   const [codelistUtils] = CodelistService()
-
   const [informationTypeList, setInformationTypeList] = useState<IInformationType[]>(
     informationTypes || []
   )
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [sort, setSort] = useState<SortState>()
 
-  const [table, sortColumn] = useTable<IInformationType, keyof IInformationType>(
-    informationTypeList,
-    {
-      sorting: informationTypeSort,
-      initialSortColumn: 'name',
+  let sortedData: IInformationType[] = informationTypeList
+
+  const comparator = (a: IInformationType, b: IInformationType, orderBy: string): number => {
+    switch (orderBy) {
+      case 'name':
+        return a.name.localeCompare(b.name)
+      case 'description':
+        return (a.description || '').localeCompare(b.description || '')
+      case 'sources':
+        return (a.sources[0]?.shortName || '').localeCompare(b.sources[0]?.shortName || '')
+      default:
+        return 0
     }
-  )
+  }
+
+  sortedData = sortedData.sort((a: IInformationType, b: IInformationType) => {
+    if (sort) {
+      return sort.direction === 'ascending'
+        ? comparator(b, a, sort.orderBy)
+        : comparator(a, b, sort.orderBy)
+    }
+    return 1
+  })
+
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   useEffect(() => {
     if (!getInfoTypes) return
-    ;(async () => {
+      ;(async () => {
       setIsLoading(true)
       setInformationTypeList(await getInfoTypes())
       setIsLoading(false)
@@ -43,47 +57,53 @@ export const InfoTypeTable = ({ informationTypes, getInfoTypes, title }: TTableP
 
   return (
     <>
-      {isLoading && <Spinner size={theme.sizing.scale1200} />}
+      {isLoading && <Loader size="large" className="flex justify-self-center" />}
       {!isLoading && (
         <div className="mb-12">
-          <HeadingXLarge>
+          <Heading size="large">
             {title} ({informationTypeList.length})
-          </HeadingXLarge>
+          </Heading>
 
           <Table
-            emptyText="Ingen opplysningstyper"
-            headers={
-              <>
-                <HeadCell title="Navn" column={'name'} tableState={[table, sortColumn]} />
-                <HeadCell
-                  title="Beskrivelse"
-                  column={'description'}
-                  tableState={[table, sortColumn]}
-                />
-                <HeadCell title="Kilder" column={'sources'} tableState={[table, sortColumn]} />
-              </>
-            }
+            size="medium"
+            sort={sort}
+            onSortChange={(sortKey) => handleSort(sort, setSort, sortKey)}
           >
-            {table.data.map((row: IInformationType, index: number) => (
-              <Row key={index}>
-                <Cell>
-                  <RouteLink href={`/informationtype/${row.id}`}>
-                    <Sensitivity sensitivity={row.sensitivity} codelistUtils={codelistUtils} />{' '}
-                    {row.name}
-                  </RouteLink>
-                </Cell>
-                <Cell>{row.description}</Cell>
-                <Cell>
-                  <DotTags
-                    list={EListName.THIRD_PARTY}
-                    codes={row.sources}
-                    linkCodelist
-                    commaSeparator
-                    codelistUtils={codelistUtils}
-                  />
-                </Cell>
-              </Row>
-            ))}
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeader sortKey="name" sortable>
+                  Navn
+                </Table.ColumnHeader>
+                <Table.ColumnHeader sortKey="description" sortable>
+                  Beskrivelse
+                </Table.ColumnHeader>
+                <Table.ColumnHeader sortKey="sources" sortable>
+                  Kilder
+                </Table.ColumnHeader>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {sortedData.map((row: IInformationType, index: number) => (
+                <Table.Row key={index}>
+                  <Table.DataCell textSize="small">
+                    <RouteLink href={`/informationtype/${row.id}`}>
+                      <Sensitivity sensitivity={row.sensitivity} codelistUtils={codelistUtils} />{' '}
+                      {row.name}
+                    </RouteLink>
+                  </Table.DataCell>
+                  <Table.DataCell textSize="small">{row.description}</Table.DataCell>
+                  <Table.DataCell textSize="small">
+                    <DotTags
+                      list={EListName.THIRD_PARTY}
+                      codes={row.sources}
+                      linkCodelist
+                      commaSeparator
+                      codelistUtils={codelistUtils}
+                    />
+                  </Table.DataCell>
+                </Table.Row>
+              ))}
+            </Table.Body>
           </Table>
         </div>
       )}
