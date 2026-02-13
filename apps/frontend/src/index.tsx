7f268c21e7b3eb0e492b6bd30463b3a4a38610c9
Andre Groseth <andregroseth@hotmail.com>
2026-02-13 11:36:15 +0100

    revert changes

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -7,26 +7,6 @@ import { Root, createRoot } from 'react-dom/client'
 import Main from './main'
 import './main.css'
 
-const cacheBustFavicon = () => {
-  const mainScriptSrc = Array.from(document.scripts)
-    .map((s) => s.src)
-    .find((src) => /\/static\/js\/main\.[a-f0-9]+\.js$/i.test(src))
-
-  const buildHash = mainScriptSrc?.match(/main\.([a-f0-9]+)\.js$/i)?.[1]
-  if (!buildHash) return
-
-  const links = Array.from(
-    document.querySelectorAll<HTMLLinkElement>('link[rel="icon"], link[rel="shortcut icon"]')
-  )
-
-  for (const link of links) {
-    const baseHref = link.href.split('?')[0]
-    link.href = `${baseHref}?v=${buildHash}`
-  }
-}
-
-cacheBustFavicon()
-
 const container: HTMLElement | null = document.getElementById('root')
 if (container) {
   const root: Root = createRoot(container)
