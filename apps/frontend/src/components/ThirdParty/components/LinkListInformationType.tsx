f09046140566434f17f20272a3d49df7eab1e632
Andre Groseth <andregroseth@hotmail.com>
2026-02-26 08:59:37 +0100

    ui: list Opplysningstyper

--- apps/frontend/src/components/ThirdParty/components/LinkListInformationType.tsx
+++ apps/frontend/src/components/ThirdParty/components/LinkListInformationType.tsx
@@ -1,24 +1,18 @@
-import { Fragment } from 'react/jsx-runtime'
 import { EObjectType, IInformationTypeShort } from '../../../constants'
 import { ObjectLink } from '../../common/RouteLink'
 
 // Disse verdiene blir sendt ned, men aldri brukt
-export const LinkListInformationType = (
-  items: IInformationTypeShort[],
-) => {
-  const len = items.length
-
+export const LinkListInformationType = (items: IInformationTypeShort[]) => {
   return (
-    <>
+    <div className="flex flex-col gap-1">
       {items.map((item: IInformationTypeShort, index: number) => (
-        <Fragment key={index}>
+        <div key={item.id ?? index}>
           <ObjectLink id={item.id} type={EObjectType.INFORMATION_TYPE}>
             {item.name}
           </ObjectLink>
-          {index < len - 1 && <span>, </span>}
-        </Fragment>
+        </div>
       ))}
-    </>
+    </div>
   )
 }
 
