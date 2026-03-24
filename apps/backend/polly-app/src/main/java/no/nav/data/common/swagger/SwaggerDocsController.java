b5458e810fa066663db35125d7ceaae6cac87b74
Andre Groseth <andregroseth@hotmail.com>
2026-03-24 07:27:09 +0100

    fix: intercept swagger api-docs response to force application/json content-type

--- apps/backend/polly-app/src/main/java/no/nav/data/common/swagger/SwaggerDocsController.java
+++ apps/backend/polly-app/src/main/java/no/nav/data/common/swagger/SwaggerDocsController.java
@@ -1,27 +1,43 @@
 package no.nav.data.common.swagger;
 
-import org.springdoc.webmvc.api.OpenApiWebMvcResource;
+import java.io.IOException;
+
+import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponseWrapper;
 
-@Hidden
-@RestController
-@RequiredArgsConstructor
-public class SwaggerDocsController {
+@Component
+@Order(1)
+public class SwaggerDocsController extends OncePerRequestFilter {
 
-    private final OpenApiWebMvcResource openApiWebMvcResource;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        if ("/v3/api-docs".equals(request.getRequestURI())) {
+            chain.doFilter(request, new HttpServletResponseWrapper(response) {
+                @Override
+                public void setContentType(String type) {
+                    super.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                }
 
-    @GetMapping(value = "/v3/api-docs", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<byte[]> apiDocs(HttpServletRequest request) throws Exception {
-        byte[] body = openApiWebMvcResource.openapiJson(request, "/v3/api-docs", java.util.Locale.getDefault());
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(body);
+                @Override
+                public void setHeader(String name, String value) {
+                    if ("Content-Type".equalsIgnoreCase(name)) {
+                        super.setHeader(name, MediaType.APPLICATION_JSON_VALUE);
+                    } else {
+                        super.setHeader(name, value);
+                    }
+                }
+            });
+        } else {
+            chain.doFilter(request, response);
+        }
     }
 }
