647e6d2ed2d7da12b81573d178dd057222241ac8
beeccy <beeccy@users.noreply.github.com>
2026-08-19 14:45:06 +0200

    NAV-30254 Fikse lint feil i NotFound

--- apps/frontend/app/not-found.tsx
+++ apps/frontend/app/not-found.tsx
@@ -1,14 +1,17 @@
 import notFound from '@/resources/notfound.svg'
 import { BodyLong } from '@navikt/ds-react'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+import { usePathname } from 'next/navigation'
 
-export default function NotFound() {
-  const router = useRouter()
+const NotFound = () => {
+  const pathName = usePathname()
+
   return (
     <div className='flex justify-center content-center mt-48'>
-      <BodyLong>Oida 404! Fant ikke den siden der nei - {router.asPath}</BodyLong>
+      <BodyLong>Oida 404! Fant ikke den siden der nei - {pathName}</BodyLong>
       <Image src={notFound} alt='404 Finner ikke den siden' style={{ maxWidth: '65%' }} />
     </div>
   )
 }
+
+export default NotFound
