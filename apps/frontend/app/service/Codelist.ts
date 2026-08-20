b4b7670071d6960c1e60d6873e501fd8e60193c7
beeccy <beeccy@users.noreply.github.com>
2026-08-20 10:06:45 +0200

    NAV-30272 Fikset Codelist med use client

--- apps/frontend/app/service/Codelist.ts
+++ apps/frontend/app/service/Codelist.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { getAllCodelists, getAllCountries, getCountriesOutsideEUEEA } from '../api/GetAllApi'
 
