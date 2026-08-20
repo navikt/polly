39c4b814ccbfe7bf1bfcfbb08eccc28001aef043
beeccy <beeccy@users.noreply.github.com>
2026-08-20 10:48:41 +0200

    NAV-30272 Fikset CodeListStyledTable med use client

--- apps/frontend/app/components/admin/CodeList/CodeListStyledTable.tsx
+++ apps/frontend/app/components/admin/CodeList/CodeListStyledTable.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { DocPencilIcon, GlassesIcon, TrashIcon } from '@navikt/aksel-icons'
 import { Button, SortState, Table, Tooltip } from '@navikt/ds-react'
 import { useEffect, useState } from 'react'
