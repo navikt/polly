9ba286e2561088d69eb1e4c5c73f97bcd8cd7645
beeccy <beeccy@users.noreply.github.com>
2026-08-25 10:54:17 +0200

    NAV-30288 Fikset noen router

--- apps/frontend/app/(pages)/process/[section]/page.tsx
+++ apps/frontend/app/(pages)/process/[section]/page.tsx
@@ -1,22 +1,25 @@
 'use client'
 
 import { getProcess } from '@/api/GetAllApi'
+import { Loader } from '@navikt/ds-react'
 import { useParams, useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
+const BNummer_RE = /[0-9]*/i
 const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
 
-export default function ProcessSectionOrRedirect() {
+const Page = () => {
   const router = useRouter()
   const { section } = useParams<{ section: string }>()
 
   useEffect(() => {
-    if (!section || !UUID_RE.test(section)) return
+    if (!section || !UUID_RE.test(section) || !BNummer_RE.test(section)) return
     getProcess(section).then((process) => {
       router.replace(`/process/purpose/${process.purposes[0].code}/${process.id}`)
     })
   }, [section, router])
 
-  // If it's a UUID we're redirecting; otherwise this path depth is not a real page.
-  return null
+  return <Loader />
 }
+
+export default Page
